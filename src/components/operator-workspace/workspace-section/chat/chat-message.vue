acea074bd41639a9bfbb5a484f5b6bd7adc7440c
dlohvinov <nsblnr@gmail.com>
2020-02-20 13:48:01 +0200

    dev: added ws section chat layouts [WTEL-1083]

---
+++ src/components/operator-workspace/workspace-section/chat/chat-message.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="message-wrap" :class="{'right': my}">
+    <img class="message-pic" src="https://cs4.pikabu.ru/post_img/2016/05/22/8/1463919617179069423.jpg" alt="client photo">
+    <div class="message">
+      <div class="message__sender" v-if="!my">Vova Lomachenko</div>
+      <p class="message__text">Ok! Thank you!</p>
+    </div>
+    <div class="message__time">10:15</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'chat-message',
+    props: {
+      // defines, if this is a user's message
+      my: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .message-wrap {
+    display: flex;
+    margin-top: 10px;
+    border: 1px solid blue;
+
+    &.right {
+      flex-direction: row-reverse;
+      text-align: right;
+    }
+  }
+
+  .message-pic {
+    flex: 0 0 50px;
+    width: 50px;
+    height: 50px;
+    border: 1px solid green;
+    border-radius: 50%;
+  }
+
+  .message {
+    margin: 0 10px;
+    background: #ef6119;
+  }
+
+  .message__time {
+    margin-top: auto;
+  }
+</style>
---
+++ src/components/operator-workspace/workspace-section/chat/chat-messages-container.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="messages-container">
+      <chat-message></chat-message>
+      <chat-message my></chat-message>
+      <chat-message></chat-message>
+      <chat-message my></chat-message>
+    </section>
+</template>
+
+<script>
+  import ChatMessage from './chat-message.vue';
+
+  export default {
+    name: 'chat-messages-container',
+    components: {
+      ChatMessage,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+</style>
