549d33e87e6755e1d0c94f02839a237c7c2dbcf8
dlohvinov <nsblnr@gmail.com>
2020-02-24 15:29:41 +0200

    dev: added "new call" button [WTEL-1094]

--- src/components/operator-workspace/queue-section/the-operator-queue-section.vue
+++ src/components/operator-workspace/queue-section/the-operator-queue-section.vue
@@ -3,22 +3,33 @@
     <call-preview></call-preview>
     <message-preview></message-preview>
     <message-preview></message-preview>
+    <rounded-action>new call</rounded-action>
   </section>
 </template>
 
 <script>
   import CallPreview from './queue-call-preview.vue';
   import MessagePreview from './queue-message-preview.vue';
+  import RoundedAction from '../../utils/rounded-action.vue';
 
   export default {
     name: 'the-operator-queue-section',
     components: {
       CallPreview,
       MessagePreview,
+      RoundedAction,
     },
   };
 </script>
 
 <style lang="scss" scoped>
+  .workspace-section {
+    position: relative;
 
+    .rounded-action {
+      position: absolute;
+      bottom: 10px;
+      left: 10px;
+    }
+  }
 </style>
