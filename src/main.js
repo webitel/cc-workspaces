01b0673bee7f87771ccf25968b71a4fe61155484
dlohvinov <nsblnr@gmail.com>
2020-03-02 17:31:55 +0200

    dev: icons added to project [WTEL-1105]

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import App from './the-app.vue';
 import router from './router';
 import store from './store';
 import i18n from './locale/i18n';
+import Icon from './components/utils/icon-wrap.vue';
 
 import './css/fonts.scss';
 import './css/main.scss';
 
 Vue.config.productionTip = false;
 
+Vue.component('icon', Icon);
 Vue.use(Vuelidate);
 new Vue({
   router,
