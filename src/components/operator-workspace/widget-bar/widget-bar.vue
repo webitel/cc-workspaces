1b47e3b7e988109998121da2fdafc60cd4d58ce0
dlohvinov <nsblnr@gmail.com>
2020-03-04 19:28:22 +0200

    dev: added widget item template, styled widgets bar template [WTEL-1114]

--- src/components/operator-workspace/widget-bar/widget-bar.vue
+++ src/components/operator-workspace/widget-bar/widget-bar.vue
@@ -1,32 +1,41 @@
 <template>
   <aside class="widget-bar">
-    <div class="widget">widget 1</div>
-    <div class="widget">widget 2</div>
-    <div class="widget">widget 3</div>
-    <div class="widget">widget 4</div>
+    <widget></widget>
+    <widget></widget>
+    <widget></widget>
+    <widget></widget>
     <div class="settings">Settings</div>
   </aside>
 </template>
 
 <script>
+  import Widget from './widget.vue';
+
   export default {
     name: 'widget-bar',
+    components: {
+      Widget,
+    },
   };
 </script>
 
 <style lang="scss" scoped>
   .widget-bar {
+    position: relative;
     display: flex;
     justify-content: center;
-    padding: 14px 20px;
+    padding: calcRem(14px) calcRem(20px);
     background: #fff;
     border-radius: $border-radius;
   }
 
   .widget {
-    margin: 0 31px; // 63px / 2
-    border: 1px solid red;
+    margin: 0 calcRem(31px); // 63px / 2
   }
 
-  .settings {}
+  .settings {
+    @extend .typo-body-md;
+    position: absolute;
+    right: calcRem(20px);
+  }
 </style>
