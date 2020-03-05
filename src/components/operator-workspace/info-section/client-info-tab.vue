85a975551ab249e61d39bfccba3a6da2403ca133
dlohvinov <nsblnr@gmail.com>
2020-03-05 12:19:01 +0200

    dev: styled info section template [WTEL-1114]

--- src/components/operator-workspace/info-section/client-info-tab.vue
+++ src/components/operator-workspace/info-section/client-info-tab.vue
@@ -1,16 +1,17 @@
 <template>
   <article class="client-info">
-    <header>
-      <span>Vova J. Lomachenko</span>
-      <a href="">CRM</a>
-    </header>
-    <ul>
-      <li><b>IVR: </b>Brakes IVR Assistant</li>
-      <li><b>Car model: </b>Tavria</li>
-      <li><b>Car model: </b>Nova</li>
-      <li><b>Client assistant: </b>Name Surname</li>
-      <li><b>Number\LDaP: </b>666</li>
-    </ul>
+<!--    <header>-->
+<!--      <span>Vova J. Lomachenko</span>-->
+<!--      <a href="">CRM</a>-->
+<!--    </header>-->
+<!--    <ul>-->
+<!--      <li><b>IVR: </b>Brakes IVR Assistant</li>-->
+<!--      <li><b>Car model: </b>Tavria</li>-->
+<!--      <li><b>Car model: </b>Nova</li>-->
+<!--      <li><b>Client assistant: </b>Name Surname</li>-->
+<!--      <li><b>Number\LDaP: </b>666</li>-->
+<!--    </ul>-->
+    <img src="../../../assets/operator-workspace/client-info-template.png" alt="">
   </article>
 </template>
 
