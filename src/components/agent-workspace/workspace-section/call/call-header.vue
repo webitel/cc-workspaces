11d866407b888c7c8978cb7bedcf3e00785677bf
dlohvinov <nsblnr@gmail.com>
2020-03-26 17:36:52 +0200

    dev: updated contacts container, added infiniteScroll mixin [WTEL-1129]
    contacts updated as in transfer component. infinite scroll logic separated as a mixin, instead of copy-paste reusing

--- src/components/agent-workspace/workspace-section/call/call-header.vue
+++ src/components/agent-workspace/workspace-section/call/call-header.vue
@@ -167,7 +167,7 @@
       },
 
       isOnHistory() {
-        return this.currentTab === 'contacts';
+        return this.currentTab === 'history';
       },
 
       isMerge() {
--- src/components/agent-workspace/workspace-section/workspace-contacts/workspace-contacts-container.vue
+++ src/components/agent-workspace/workspace-section/workspace-contacts/workspace-contacts-container.vue
@@ -1,43 +1,60 @@
 <template>
   <div class="ws-worksection">
-    <search v-model="search"></search>
-    <div class="ws-worksection__list">
-      <div>
-        <div class="ws-worksection__list-separator">A</div>
-        <contact></contact>
-        <contact></contact>
-      </div>
-      <div>
-        <div class="ws-worksection__list-separator">B</div>
-        <contact></contact>
-        <contact></contact>
-      </div>
+    <search
+      v-model="search"
+      @search="loadDataList"
+    />
+    <div class="ws-worksection__list" ref="scroll-wrap">
+     <contact
+        v-for="(item, key) of dataList"
+        :class="{'selected': item === selected}"
+        :id="`scroll-item-${key}`"
+        :key="key"
+        :item="item"
+        @click.native="select(item)"
+      ></contact>
+      <observer
+        :options="obsOptions"
+        @intersect="handleIntersect"/>
     </div>
   </div>
 </template>
 
 <script>
-  import Search from '../../../utils/search-input.vue';
+  import { getUsersList } from '../../../../api/agent-workspace/users';
+  import infiniteScrollMixin from '../../../../mixins/infiniteScrollMixin';
   import Contact from './workspace-contact.vue';
 
   export default {
     name: 'workspace-contacts-container',
+    mixins: [infiniteScrollMixin],
     components: {
-      Search,
       Contact,
     },
 
     data: () => ({
-      search: '',
+      selected: null,
     }),
+
+    methods: {
+      select(item) {
+        this.selected = item;
+      },
+
+      async loadDataList() {
+        const response = await getUsersList(this.page, this.size, this.search);
+        this.dataList = [...this.dataList, ...response];
+      },
+    },
   };
 </script>
 
 <style lang="scss" scoped>
   .ws-contact-item {
     cursor: pointer;
-    &:hover {
-      background: $page-bg-color;
+
+    &.selected, &:hover {
+      border-color: $accent-color;
     }
   }
 </style>
