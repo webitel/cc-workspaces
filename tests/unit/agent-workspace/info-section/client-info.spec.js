f21378314da6593aa2a3f630e413b3ea32f7d53b
dlohvinov <nslbnr@gmail.com>
2020-04-02 15:06:00 +0300

    test: added tests for member variables md in client info section [WTEL-1132]

--- tests/unit/agent-workspace/info-section/client-info.spec.js
+++ tests/unit/agent-workspace/info-section/client-info.spec.js
@@ -8,7 +8,7 @@ import WorkspaceStates
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-describe('Client Info MD', () => {
+describe('Client Info MD from Call', () => {
   let state;
   let store;
 
@@ -44,7 +44,7 @@ describe('Client Info MD', () => {
     expect(md.isEmpty()).toBe(true);
   });
 
-  it('Correctly renders key-value in payload', () => {
+  it('Correctly renders key-value in call payload', () => {
     state.callOnWorkspace.payload = {
       key: 'value',
     };
@@ -57,7 +57,7 @@ describe('Client Info MD', () => {
     expect(md.find('p').text()).toBe('value');
   });
 
-  it('Correctly renders key-value with MD in payload', () => {
+  it('Correctly renders key-value with MD in call payload', () => {
     state.callOnWorkspace.payload = {
       md: '# h1 Heading',
     };
@@ -69,5 +69,58 @@ describe('Client Info MD', () => {
     expect(md.find('h3').text()).toBe('md:');
     expect(md.find('h1').text()).toBe('h1 Heading');
   });
-})
-;
+});
+
+describe('Client Info MD from Member', () => {
+  let state;
+  let store;
+
+  beforeEach(() => {
+    state = {
+      memberOnWorkspace: {
+        variables: {},
+      },
+    };
+    store = new Vuex.Store({
+      state,
+      modules: {
+        workspace: {
+          namespaced: true,
+          state: {
+            workspaceState: WorkspaceStates.MEMBER,
+          },
+        },
+        member: {
+          namespaced: true,
+          state,
+        },
+      },
+    });
+  });
+
+  it('Correctly renders key-value in member variables', () => {
+    state.memberOnWorkspace.variables = {
+      key: 'value',
+    };
+    const wrapper = shallowMount(ClientInfo, {
+      store,
+      localVue,
+    });
+    const md = wrapper.find('#md');
+    expect(md.find('h3').text()).toBe('key:');
+    expect(md.find('p').text()).toBe('value');
+  });
+
+  it('Correctly renders key-value with MD in member variables', () => {
+    state.memberOnWorkspace.variables = {
+      md: '# h1 Heading',
+    };
+    const wrapper = shallowMount(ClientInfo, {
+      store,
+      localVue,
+    });
+    const md = wrapper.find('#md');
+    expect(md.find('h3').text()).toBe('md:');
+    expect(md.find('h1').text()).toBe('h1 Heading');
+  });
+});
