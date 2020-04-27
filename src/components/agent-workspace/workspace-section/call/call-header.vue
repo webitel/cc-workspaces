c8d637d8891fd253a761fb52f3defa58c898ec1a
Daniil Lohvinov <nsblnr@gmail.com>
2020-04-27 12:56:29 +0300

    fix: removed PREVIEW and ACTIVE call states, preview computes from answered At call property [WTEL-829]

--- src/components/agent-workspace/workspace-section/call/call-header.vue
+++ src/components/agent-workspace/workspace-section/call/call-header.vue
@@ -155,6 +155,7 @@
     computed: {
       ...mapState('call', {
         call: (state) => state.callOnWorkspace,
+        callList: (state) => state.callList,
         callState: (state) => state.callState,
       }),
 
@@ -180,22 +181,20 @@
       },
 
       isBridge() {
-        return (this.callState !== CallActions.Hangup
-          || this.callState !== CallActions.Ringing)
-          && this.$store.state.call.callList.filter((call) => (
+        return (this.call.state !== CallActions.Hangup
+          || this.call.state !== CallActions.Ringing)
+          && this.callList.filter((call) => (
             call.state !== CallActions.Hangup
             || call.state !== CallActions.Ringing
           )).length > 1;
       },
 
       isTransfer() {
-        return this.callState === CallStates.ACTIVE
-          || this.callState === CallStates.TRANSFER;
+        return this.callState !== CallStates.NEW;
       },
 
       isHangup() {
-        return this.callState === CallStates.ACTIVE
-          || this.callState === CallStates.TRANSFER;
+        return this.callState !== CallStates.NEW;
       },
 
       isCall() {
