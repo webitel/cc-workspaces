4337ba09517193f53b89e902030c19d4c86cc66b
Daniil Lohvinov <nsblnr@gmail.com>
2020-05-05 20:05:34 +0300

    dev: added filters for truncating too long text, registered globally [WTEL-829]

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router';
 import store from './store';
 import i18n from './locale/i18n';
 import Icon from './components/utils/icon-wrap.vue';
+import './filters/globalFilters';
 
 import './css/fonts.scss';
 import './css/main.scss';
