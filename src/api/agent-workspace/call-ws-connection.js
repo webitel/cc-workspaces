3cdde6cbbc7b1a2eb33ab4314a1a0fe694d038ce
Daniil Lohvinov <nsblnr@gmail.com>
2020-05-12 19:12:45 +0300

    dev: changed websocket url [WTEL-829]

--- src/api/agent-workspace/call-ws-connection.js
+++ src/api/agent-workspace/call-ws-connection.js
@@ -1,6 +1,8 @@
 import { Client } from 'webitel-sdk';
 
-const BASE_URL = 'wss://dev.webitel.com/ws';
+// eslint-disable-next-line no-restricted-globals
+const BASE_URL = process.env.NODE_ENV === 'production' ? `${location.origin.replace(/^http/, 'ws')}/ws` : 'wss://dev.webitel.com/ws';
+// const BASE_URL = 'wss://dev.webitel.com/ws';
 // const BASE_URL = 'ws://10.10.10.25:10025';
 const token = localStorage.getItem('access-token');
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 process.env.VUE_APP_API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'https://dev.webitel.com/api';
+process.env.VUE_APP_WSS_URL = process.env.NODE_ENV === 'production' ? '/ws' : 'wss://dev.webitel.com/ws';
 process.env.VUE_APP_AUTH_MODULE_URL = process.env.NODE_ENV === 'production' ? '/app/auth' : 'https://dev.webitel.com/app/auth';
 
 module.exports = {
