1aa8a4400b7f178f6586c68762bff926f7c72fe8
Daniil Lohvinov <nsblnr@gmail.com>
2020-05-15 12:20:28 +0300

    dev: added icons, updated preferences component [WTEL-1216]

--- src/components/cc-header/user-preferences.vue
+++ src/components/cc-header/user-preferences.vue
@@ -2,7 +2,8 @@
   <div class="user-preferences" v-clickaway="close">
     <button
       class="icon-btn user-preferences__account"
-      @click.prevent="showUserActions = !showUserActions"
+      :class="{'opened': isOpened}"
+      @click.prevent="isOpened = !isOpened"
       v-clickaway="close"
     >
       <icon>
@@ -11,32 +12,58 @@
         </svg>
       </icon>
     </button>
-    <ul
-      class="user-preferences__actions-wrap"
-      v-show="showUserActions"
-    >
-      <li class="user-preferences__action user-preferences__action__docs">
-        <!--        <i class="icon-icon_doc"></i>-->
-        <a
-          class="user-preferences__action__text"
-          href="https://docs.webitel.com/display/WEP/Webitel+Elastic+Platform"
-          target="_blank"
-          @click="close"
-        >
-          {{$t('header.docs')}}
-        </a>
-      </li>
-      <li class="user-preferences__action user-preferences__action__logout">
-        <!--        <i class="icon-icon_logout"></i>-->
-        <span class="user-preferences__action__text" @click="logoutUser">
+    <section class="user-preferences__actions-wrapper" v-show="isOpened">
+      <header class="user-preferences__actions-header">
+        <h3 class="user-preferences__username">{{name}}</h3>
+        <p class="user-preferences__account">{{username}}</p>
+      </header>
+      <ul class="user-preferences__actions">
+        <li class="user-preferences__action user-preferences__action__docs">
+          <a
+            class="user-preferences__action__link"
+            href="https://docs.webitel.com/display/WEP/Webitel+Elastic+Platform"
+            target="_blank"
+            @click="close"
+          >
+            <icon>
+              <svg class="icon sm">
+                <use xlink:href="#icon-docs-sm"></use>
+              </svg>
+            </icon>
+            <span>{{$t('header.docs')}}</span>
+          </a>
+        </li>
+        <li class="user-preferences__action user-preferences__action__settings">
+          <a class="user-preferences__action__link" @click.prevent="settings">
+            <icon>
+              <svg class="icon sm">
+                <use xlink:href="#icon-settings-sm"></use>
+              </svg>
+            </icon>
+            <span>
+          {{$t('reusable.settings')}}
+        </span>
+          </a>
+        </li>
+        <li class="user-preferences__action user-preferences__action__logout">
+          <a class="user-preferences__action__link" @click.prevent="logoutUser">
+            <icon>
+              <svg class="icon sm">
+                <use xlink:href="#icon-logout-sm"></use>
+              </svg>
+            </icon>
+            <span>
           {{$t('reusable.logout')}}
         </span>
-      </li>
-    </ul>
+          </a>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   import clickaway from '../../directives/clickaway';
   import { logout } from '../../api/auth/auth';
 
@@ -44,71 +71,115 @@
     name: 'user-preferences',
     directives: { clickaway },
     data: () => ({
-      showUserActions: false,
+      isOpened: false,
     }),
+
+    computed: {
+      ...mapState('userinfo', {
+        username: (state) => state.username,
+      }),
+
+      name() {
+        return this.username.split('@').shift();
+      },
+    },
 
     methods: {
       settings() {
       },
 
       logoutUser() {
-        this.close();
         logout();
+        this.close();
       },
 
       close() {
-        this.showUserActions = false;
+        this.isOpened = false;
       },
     },
   };
 </script>
 
 <style lang="scss" scoped>
+  $user-preferences-gap: calcVH(30px);
+  $user-preferences-shadow: 0px calcVH(8px) calcVH(18px) rgba(0, 0, 0, 0.08);
+
+  .typo-user-preferences-title {
+    font-family: 'Montserrat Regular', monospace;
+    font-size: calcVH(14px);
+    line-height: calcVH(17px);
+  }
+
+  .typo-user-preferences-subtitle {
+    font-family: 'Montserrat Regular', monospace;
+    font-size: calcVH(14px);
+    line-height: calcVH(17px);
+  }
+
+  .typo-user-preferences-action {
+    font-family: 'Montserrat Regular', monospace;
+    font-size: calcVH(13px);
+    line-height: calcVH(16px);
+  }
+
   .user-preferences {
     position: relative;
     display: flex;
     align-items: center;
-  }
-
-  .user-preferences__account {
     margin-left: calcVH(30px);
-    cursor: pointer;
+    z-index: 90;
   }
 
-  .user-preferences__actions-wrap {
+  .user-preferences__actions-wrapper {
     @extend .typo-body-md;
     @extend .box-shadow;
     position: absolute;
-    top: calcVH(24px);
+    top: calc(100% + 5px); // icon + 5px
     right: 0;
     min-width: calcVH(120px);
-    margin-top: calcVH(8px);
-    color: #000;
     background: #fff;
     border-radius: $border-radius;
-    box-shadow: $box-shadow;
-    z-index: 100;
+    box-shadow: $user-preferences-shadow;
+    transition: $transition;
+  }
 
-    .user-preferences__action {
+  .user-preferences__actions-header {
+    padding: $user-preferences-gap $user-preferences-gap calcVH(21px);
+
+    .user-preferences__username {
+      @extend .typo-user-preferences-title;
+    }
+
+    .user-preferences__account {
+      @extend .typo-user-preferences-subtitle;
+    }
+  }
+
+  .user-preferences__action {
+    display: flex;
+
+    &:hover {
+      background: $page-bg-color;
+    }
+
+    .user-preferences__action__link {
+      @extend .typo-user-preferences-action;
       display: flex;
       align-items: center;
-      transition: $transition;
+      width: 100%;
+      padding: calcVH(11px) $user-preferences-gap;
       cursor: pointer;
 
-      .user-preferences__action__text {
-        @extend .typo-body-sm;
-        display: block;
-        width: 100%;
-        padding: calcVH(11px) calcVH(13px);
-        margin-left: calcVH(8px);
+      .icon-wrap {
+        margin-right: calcVH(8px);
       }
+    }
 
-      &__logout {
-        color: $false-color;
-      }
-
-      &:hover {
-        background: $page-bg-color;
+    &__logout .user-preferences__action__link {
+      color: $false-color;
+      .icon {
+        fill: $false-color;
+        stroke: $false-color;
       }
     }
   }
