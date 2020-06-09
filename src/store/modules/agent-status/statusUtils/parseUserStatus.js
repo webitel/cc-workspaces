574341259ae55e7edfeb7ddf5936c67fb19ae2b7
Daniil Lohvinov <nsblnr@gmail.com>
2020-06-09 14:53:29 +0300

    done [WTEL-1283]

--- src/store/modules/agent-status/statusUtils/parseUserStatus.js
+++ src/store/modules/agent-status/statusUtils/parseUserStatus.js
@@ -2,8 +2,8 @@ import UserStatus from './UserStatus';
 
 const parseUserStatus = (presence) => {
   if (!presence || !presence.status) return UserStatus.OFFLINE;
-  if (presence.status.includes('dlg')) return UserStatus.BUSY;
   if (presence.status.includes('dnd')) return UserStatus.DND;
+  if (presence.status.includes('dlg')) return UserStatus.BUSY;
   if (presence.status.includes('sip')) return UserStatus.ACTIVE;
   return '';
 };
