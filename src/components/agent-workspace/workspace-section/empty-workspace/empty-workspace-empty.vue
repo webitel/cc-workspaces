2a070ae201fa412ba8d1f491183cc9491c56d43e
Daniil Lohvinov <nsblnr@gmail.com>
2020-06-17 10:42:56 +0300

    dev: added empty workspace animation [WTEL-1286]

--- src/components/agent-workspace/workspace-section/empty-workspace/empty-workspace-empty.vue
+++ src/components/agent-workspace/workspace-section/empty-workspace/empty-workspace-empty.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="empty-workspace__content-wrap">
-    <img
-      class="workspace-pic"
-      src="../../../../assets/agent-workspace/empty-ws/empty-ws-empty.svg"
-      alt="workspace is empty"
-    >
+    <div class="workspace-pic">
+      <iframe
+        :src="animationUrl"
+      ></iframe>
+    </div>
     <h1 class="empty-workspace__heading">{{$t('emptyWorkspace.empty.heading')}}</h1>
     <p class="empty-workspace__text">{{$t('emptyWorkspace.empty.text')}}</p>
   </div>
@@ -13,6 +13,12 @@
 <script>
   export default {
     name: 'empty-workspace-empty',
+    computed: {
+      animationUrl() {
+        const baseUrl = process.env.BASE_URL; // to resolve iframe equalizer path after build
+        return `${baseUrl}animations/ws-empty/index.html`;
+      },
+    },
   };
 </script>
 
