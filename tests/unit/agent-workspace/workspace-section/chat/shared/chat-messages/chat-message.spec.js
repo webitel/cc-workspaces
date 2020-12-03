bcec2210b95af4d67a3f502790160d927e30d94b
dlohvinov <nsblnr@gmail.com>
2020-12-03 11:30:06 +0200

    test: added chat message and chat message container ui tests [WTEL-1562]

---
+++ tests/unit/agent-workspace/workspace-section/chat/shared/chat-messages/chat-message.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount } from '@vue/test-utils';
+import ChatMessage
+  from '../../../../../../../src/components/agent-workspace/workspace-section/chat/shared/chat-messages/chat-message.vue';
+
+let message = {};
+describe('Chat Message component', () => {
+  beforeEach(() => {
+    message = {};
+  });
+
+  it('renders a component', () => {
+    const wrapper = shallowMount(ChatMessage, { propsData: { message } });
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('correctly computes sentAt time', () => {
+    message.createdAt = '0';
+    const wrapper = shallowMount(ChatMessage, { propsData: { message } });
+    expect(wrapper.vm.sentAt).toBe('03:00 AM');
+  });
+
+  it('correctly computes message text', () => {
+    const text = 'jest';
+    message.value = text;
+    const wrapper = shallowMount(ChatMessage, { propsData: { message } });
+    expect(wrapper.vm.text).toBe(text);
+  });
+});
