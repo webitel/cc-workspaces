a246cc26883bdfd0211530a2b363c63d6efc6fde
dlohvinov <nsblnr@gmail.com>
2021-04-27 09:39:58 +0300

    fix: fixed history api repository usage [WTEL-1862]

--- src/api/agent-workspace/history/HistoryAPIRepository.js
+++ src/api/agent-workspace/history/HistoryAPIRepository.js
@@ -1,74 +1,37 @@
 import { CallServiceApiFactory } from 'webitel-sdk';
+import { SdkListGetterApiConsumer } from 'webitel-sdk/esm2015/api-consumers';
 import configuration from '../../openAPIConfig';
 import instance from '../../instance';
 
-const defaultParams = {
-  page: 1,
-  size: 10,
-  search: '',
-  createdAtFrom: 0,
-  createdAtTo: new Date().setHours(23, 59, 59, 999), // today end
-  sort: '-created_at',
-};
 const callService = new CallServiceApiFactory(configuration, '', instance);
 
-const fetchHistory = async ({
-                              page,
-                              size,
-                              createdAtFrom,
-                              createdAtTo,
-                              userId,
-                              memberId,
-                              q,
-                              cause,
-                              fields,
-                              sort,
-                              direction,
-                              isMissed,
-                            }) => {
-  try {
-    const response = await callService.searchHistoryCall(
-      page,
-      size,
-      createdAtFrom,
-      createdAtTo,
-      userId,
-      undefined,
-      undefined,
-      undefined,
-      memberId,
-      undefined,
-      q,
-      undefined,
-      undefined,
-      undefined,
-      undefined,
-      cause,
-      undefined,
-      fields,
-      sort,
-      undefined,
-      undefined,
-      direction,
-      undefined,
-      undefined,
-      isMissed,
-    );
-    return { next: !!response.next, items: response.items || [] };
-  } catch (err) {
-    throw err;
-  }
+const _getHistory = (getList) => function ({
+                                             page,
+                                             size,
+                                             search,
+                                             sort = '-created_at',
+                                             fields,
+                                             createdAtFrom = 0,
+                                             createdAtTo = new Date().setHours(23, 59, 59, 999), // today end
+                                             userId,
+                                             memberId,
+                                             cause,
+                                             direction,
+                                             isMissed,
+                                           }) {
+  const params = [page, size, search, sort, fields, createdAtFrom, createdAtTo, userId, undefined,
+    undefined, undefined, memberId, undefined, undefined, undefined, undefined, undefined,
+    cause, undefined, undefined, undefined, direction, undefined, undefined, isMissed];
+  return getList(params);
 };
 
+const listGetter = new SdkListGetterApiConsumer(callService.searchHistoryCall)
+  .setGetListMethod(_getHistory);
+
+const getHistory = (params) => listGetter.getList(params);
+
 const historyAPIRepository = {
-  async getHistory(argParams) {
-    const params = {
-      ...defaultParams,
-      ...argParams,
-    };
-    params.q = params.search;
-    return fetchHistory(params);
-  },
+  getHistory,
 };
 
 export default historyAPIRepository;
