1697f44a747311a425c9491a6b3d0d952317bb50
Roman <romanzaritskyi@gmail.com>
2021-10-19 12:54:28 +0300

    refactor: according to review [WPRO-189]

--- src/components/agent-workspace/info-section/general-info/agent-queues.vue
+++ src/components/agent-workspace/info-section/general-info/agent-queues.vue
@@ -1,45 +1,27 @@
 <template>
   <article class="agent-queues">
-    <!--    Headers-->
+
     <div class="agent-queues__headers">
       <div
         class="agent-queues__headers-item"
         v-for="(header, index) in headers"
         :key="index"
       >
-        {{ $t(`${header.locale}`) }}
+        {{ $t(header.locale) }}
       </div>
     </div>
-    <!--    Sub headers-->
-    <div class="agent-queues__sub-headers">
-      <div
-        class="agent-queues__sub-headers-item"
-        v-for="(subHeader, index) in subHeaders"
-        :key="index"
-      >
-        {{ $t(`${subHeader.locale}`) }}
-      </div>
-    </div>
-    <!--    Queues-->
+
     <div
       class="agent-queues__queue"
       v-for="queue in queues"
       :key="queue.queue.id"
     >
       <div class="agent-queues__queue-item">{{ queue.queue.name }}</div>
-      <div class="agent-queues__queue-item">{{ queue.agents.total }}</div>
-      <div class="agent-queues__queue-item">
-        <wt-indicator
-          color="primary"
-          :text="queue.agents.pause"
-        ></wt-indicator>
-      </div>
-      <div class="agent-queues__queue-item">
-        <wt-indicator
-          color="success"
-          :text="queue.agents.free"
-        ></wt-indicator>
-      </div>
+
+      <table-agents
+        :agents="queue.agents"
+      ></table-agents>
+
       <div class="agent-queues__queue-item">
         <wt-badge>{{ queue.waitingMembers }}</wt-badge>
       </div>
@@ -48,14 +30,16 @@
 </template>
 
 <script>
-import headers from '../../../../store/modules/agent-info/_internals/headers';
-import subHeaders from '../../../../store/modules/agent-info/_internals/subHeaders';
+import tableAgents from './table-agents.vue';
+import headers from '../../../../store/modules/agent-info/_internals/agentQueuesHeaders';
 
 export default {
   name: 'agent-queues',
+  components: {
+    tableAgents,
+  },
   data: () => ({
     headers,
-    subHeaders,
   }),
   props: {
     queues: {
@@ -78,33 +62,21 @@ export default {
   justify-items: center;
 }
 
-.agent-queues__sub-headers {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  align-items: center;
-  justify-items: center;
-
-  .agent-queues__sub-headers-item:first-child {
-      grid-column-start: 2;
-    }
-}
-
 .agent-queues__queue {
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  align-items: center;
+  grid-template-columns: 1fr 3fr 1fr;
+  align-items: start;
   margin-top: var(--component-spacing);
 
   .agent-queues__queue-item {
 
-    & + .agent-queues__queue-item {
-      @extend %typo-strong-lg;
+    &:last-of-type {
       justify-self: center;
+    }
+  }
 
-    }
-    .wt-badge {
-      @extend %typo-strong-sm;
-    }
+  .wt-badge {
+    @extend %typo-strong-sm;
   }
 }
 </style>
