d0fa28b33fd7689ca25fc4be39338519f91f7d14
dlohvinov <nsblnr@gmail.com>
2022-03-26 16:57:18 +0200

    refactor: chat-message component split into logical chunks [WTEL-2320]

---
+++ src/components/agent-workspace/workspace-section/chat/chat-messaging-container/chat-messages/message/chat-message-image.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="image" class="chat-message-image" @click="$emit('open', image)">
+    <img
+      class="chat-message-image__img"
+      :src="image.url"
+      :alt="image.name">
+  </div>
+</template>
+
+<script>
+import chatMessageDetailMixin from '../../../../../../../mixins/chatMessageDetailMixin';
+
+export default {
+  name: 'chat-message-image',
+  mixins: [chatMessageDetailMixin],
+  computed: {
+    image() {
+      const isImage = this.message.file && this.message.file.mime.includes('image');
+      return isImage ? this.message.file : null;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.chat-message-image {
+  cursor: pointer;
+
+  &__img {
+    @extend %typo-body-md;
+    width: 100%;
+  }
+}
+</style>
