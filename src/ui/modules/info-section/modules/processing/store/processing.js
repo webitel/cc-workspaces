e8d1a78394c4663a35027dfda47cf8bb95d4c3d4
dlohvinov <nsblnr@gmail.com>
2022-05-09 14:46:54 +0300

    feature: initializing fields with default values [WTEL-2528]

--- src/ui/modules/info-section/modules/processing/store/processing.js
+++ src/ui/modules/info-section/modules/processing/store/processing.js
@@ -1,4 +1,20 @@
 const actions = {
+  SEND_FORM: (
+    context,
+    { action, task = context.rootGetters.TASK_ON_WORKSPACE } = {},
+  ) => {
+    const form = task.task.form.body.reduce((form, { id, value }) => ({
+      ...form,
+      [id]: value,
+    }), {});
+    return task.task.formAction(action.id, form);
+  },
+  RENEW_PROCESSING_TIME: (
+    context,
+    { task = context.rootGetters.TASK_ON_WORKSPACE },
+  ) => (
+    task.task.renew()
+  ),
 };
 
 export default {
