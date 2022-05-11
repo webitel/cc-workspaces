161ac22ad668e5eb73fc309071b3569b63f8197f
dlohvinov <nsblnr@gmail.com>
2022-05-11 14:39:44 +0300

    fix: groomed processing form collecting before next [WTEL-2528]

--- src/ui/modules/info-section/modules/processing/store/processing.js
+++ src/ui/modules/info-section/modules/processing/store/processing.js
@@ -3,10 +3,15 @@ const actions = {
     context,
     { action, task = context.rootGetters.TASK_ON_WORKSPACE } = {},
   ) => {
-    const form = task.task.form.body.reduce((form, { id, value }) => ({
-      ...form,
-      [id]: value,
-    }), {});
+    const form = task.task.form.body.reduce((form, { id, value, view }) => {
+      let _value = value;
+      if (view.component === 'form-text') return form;
+      if (view.component === 'wt-select' && typeof value === 'object') _value = value.value;
+      return {
+        ...form,
+        [id]: _value,
+      };
+    }, {});
     return task.task.formAction(action.id, form);
   },
   RENEW_PROCESSING_TIME: (
