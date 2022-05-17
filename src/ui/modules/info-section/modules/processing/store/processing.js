d35e828f0d38ebd142f0e0b364f6c10484e0ed78
dlohvinov <nsblnr@gmail.com>
2022-05-17 10:20:11 +0300

    fix: fixed select multiple value [WTEL-2528]

--- src/ui/modules/info-section/modules/processing/store/processing.js
+++ src/ui/modules/info-section/modules/processing/store/processing.js
@@ -3,10 +3,21 @@ const actions = {
     context,
     { action, task = context.rootGetters.TASK_ON_WORKSPACE } = {},
   ) => {
-    const form = task.task.form.body.reduce((form, { id, value }) => ({
-      ...form,
-      [id]: value,
-    }), {});
+    const form = task.task.form.body.reduce((form, { id, value, view }) => {
+      let _value = value;
+      if (view.component === 'form-text') return form;
+      if (view.component === 'wt-select') {
+        if (Array.isArray(value)) {
+          _value = value.map((val) => (
+            typeof value === 'object' ? val.value : val
+          ));
+        } else if (typeof value === 'object') _value = value.value;
+      }
+      return {
+        ...form,
+        [id]: _value,
+      };
+    }, {});
     return task.task.formAction(action.id, form);
   },
   RENEW_PROCESSING_TIME: (
