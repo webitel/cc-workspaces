45c2ea80cc53c92abb17fc86cc0a212b86825e8a
dlohvinov <nsblnr@gmail.com>
2022-06-09 18:20:00 +0300

    refactor: base of notifications module moved to webitel-ui [WTEL-2564]

--- src/features/modules/notifications/store/__tests__/notifications.spec.js
+++ src/features/modules/notifications/store/__tests__/notifications.spec.js
@@ -1,5 +1,4 @@
-import { ChatActions } from 'webitel-sdk';
-import audio from '../../../../../../public/media/new-message.wav';
+import { CallActions, ChatActions } from 'webitel-sdk';
 import notificationsModule from '../notifications';
 import '../../../../../../tests/unit/mocks/broadcastChannelMock';
 
@@ -32,21 +31,6 @@ describe('features/notifications store: actions', () => {
     context.commit.mockClear();
   });
 
-  it('INITIALIZE action dispatches _SETUP_THIS_TAB_ID action', () => {
-    notificationsModule.actions.INITIALIZE(context);
-    expect(context.dispatch.mock.calls[0][0]).toContain('_SETUP_THIS_TAB_ID');
-  });
-
-  it('INITIALIZE action dispatches _SETUP_UNREAD_COUND_BROADCAST_LISTENING action', () => {
-    notificationsModule.actions.INITIALIZE(context);
-    expect(context.dispatch.mock.calls[1][0]).toContain('_SETUP_UNREAD_COUND_BROADCAST_LISTENING');
-  });
-
-  it('INITIALIZE action commits _SETUP_THIS_TAB_ID action', () => {
-    notificationsModule.actions.INITIALIZE(context);
-    expect(context.dispatch).toHaveBeenCalledWith('_SETUP_THIS_TAB_ID');
-  });
-
   it('HANDLE_CHAT_EVENT action dispatches _PLAY_NOTIFICATION_SOUND action', () => {
     notificationsModule.actions.HANDLE_CHAT_EVENT(context, { action: ChatActions.Message, chat });
     expect(context.dispatch.mock.calls[0][0]).toContain('PLAY_SOUND');
@@ -63,67 +47,9 @@ describe('features/notifications store: actions', () => {
     expect(context.dispatch.mock.calls[0][0]).not.toContain('SEND_NOTIFICATION');
   });
 
-  it('INCREMENT_UNREAD_COUNT action dispatches _SET_UNREAD_COUNT and increases unreadCount', () => {
-    notificationsModule.actions.INCREMENT_UNREAD_COUNT(context);
-    expect(context.dispatch).toHaveBeenCalledWith('_SET_UNREAD_COUNT', context.state.unreadCount + 1);
-  });
-
-  it('_SETUP_UNREAD_COUND_BROADCAST_LISTENING action commits SET_BROADCAST_CHANNEL mutation', () => {
-    notificationsModule.actions._SETUP_UNREAD_COUND_BROADCAST_LISTENING(context);
-    expect(context.commit.mock.calls[0][0]).toContain('SET_BROADCAST_CHANNEL');
-  });
-
-  it('PLAY_SOUND action commits SET_CURRENTLY_PLAIYNG mutation with sound', async () => {
-    const sound = new Audio(audio);
-    await notificationsModule.actions.PLAY_SOUND(context, { sound });
-    expect(context.commit).toHaveBeenCalledWith('SET_CURRENTLY_PLAYING', sound);
-  });
-
-  it('_SET_UNREAD_COUNT dispatches _SET_TAB_TITLE action', () => {
-    const unreadCount = 5;
-    notificationsModule.actions._SET_UNREAD_COUNT(context, unreadCount);
-    expect(context.dispatch).toHaveBeenCalledWith('_SET_TAB_TITLE');
-  });
-
-  it('_SET_UNREAD_COUNT action commits _SET_UNREAD_COUNT mutation with count', () => {
-    const unreadCount = 5;
-    notificationsModule.actions._SET_UNREAD_COUNT(context, unreadCount);
-    expect(context.commit).toHaveBeenCalledWith('SET_UNREAD_COUNT', unreadCount);
-  });
-
-  it('_RESET_UNREAD_COUNT action does not dispatch if unread count is 0', () => {
-    notificationsModule.actions._RESET_UNREAD_COUNT(context);
-    expect(context.dispatch).not.toHaveBeenCalledWith('_SET_UNREAD_COUNT');
-  });
-
-  it('_RESET_UNREAD_COUNT action dispatches _SET_UNREAD_COUNT passing unread count equal to 0', () => {
-    context.state.unreadCount = 1;
-    notificationsModule.actions._RESET_UNREAD_COUNT(context);
-    expect(context.dispatch).toHaveBeenCalledWith('_SET_UNREAD_COUNT', 0);
-  });
-
   it('HANDLE_ANY_CALL_RINGING action dispatches PLAY_SOUND action with sound audio', () => {
-    const sound = new Audio(audio);
-    sound.loop = true;
     notificationsModule.actions.HANDLE_ANY_CALL_RINGING(context);
-    expect(context.dispatch).toHaveBeenCalledWith('PLAY_SOUND', { sound });
-  });
-
-  it('PLAY_SOUND action sets localStorage wtIsPlaying to true', () => {
-    const sound = new Audio(audio);
-    notificationsModule.actions.PLAY_SOUND(context, { sound });
-    expect(localStorage.getItem('wtIsPlaying')).toBeTruthy();
-  });
-
-  it('STOP_SOUND action commits RESET_CURRENTLY_PLAYING mutation', () => {
-    notificationsModule.actions.STOP_SOUND(context);
-    expect(context.commit).toHaveBeenCalledWith('RESET_CURRENTLY_PLAYING');
-  });
-
-  it('STOP_SOUND action removes localStorage wtIsPlaying', () => {
-    localStorage.setItem('wtIsPlaying', 'true');
-    notificationsModule.actions.STOP_SOUND(context);
-    expect(localStorage.getItem('wtIsPlaying')).toBeFalsy();
+    expect(context.dispatch).toHaveBeenCalledWith('PLAY_SOUND', { action: CallActions.Ringing });
   });
 
   it('HANDLE_CALL_START action sets localStorage wtIsPlaying', async () => {
@@ -145,37 +71,5 @@ describe('features/notifications store: actions', () => {
   it('HANDLE_CALL_END action commits SET_CURRENTLY_PLAYING mutation', () => {
     notificationsModule.actions.HANDLE_CALL_END(context);
     expect(context.commit).toHaveBeenCalledWith('SET_CURRENTLY_PLAYING', null);
-  });
-});
-
-describe('features/notifications store: mutations', () => {
-  it('SET_THIS_TAB_ID sets window id to state', () => {
-    const thisTabId = 'thisTabId';
-    notificationsModule.mutations.SET_THIS_TAB_ID(state, thisTabId);
-    expect(state.thisTabId).toBe(thisTabId);
-  });
-
-  it('SET_BROADCAST_CHANNEL sets broadcastChannel to state', () => {
-    const state = { broadcastChannel: null };
-    const channel = new BroadcastChannel('test channel');
-    notificationsModule.mutations.SET_BROADCAST_CHANNEL(state, channel);
-    expect(state.broadcastChannel).toEqual(channel);
-  });
-
-  it('SET_CURRENTLY_PLAIYNG mutation sets currentlyPlaying to state', () => {
-    const wtIsPlaying = true;
-    notificationsModule.mutations.SET_CURRENTLY_PLAYING(state, wtIsPlaying);
-    expect(state.currentlyPlaying).toBe(true);
-  });
-
-  it('RESET_CURRENTLY_PLAYING mutation sets currentlyPlaying to false in state', () => {
-    notificationsModule.mutations.RESET_CURRENTLY_PLAYING(state);
-    expect(state.currentlyPlaying).toBe(false);
-  });
-
-  it('_SET_UNREAD_COUNT mutation sets unreadCount to state', () => {
-    const unreadCount = 15;
-    notificationsModule.mutations.SET_UNREAD_COUNT(state, unreadCount);
-    expect(state.unreadCount).toEqual(unreadCount);
   });
 });
--- src/features/modules/notifications/store/notifications.js
+++ src/features/modules/notifications/store/notifications.js
@@ -1,45 +1,10 @@
+import NotificationsStoreModule
+  from '@webitel/ui-sdk/src/modules/Notifications/store/NotificationsStoreModule';
 import { snakeToCamel } from '@webitel/ui-sdk/src/scripts/caseConverters';
-import { ChatActions } from 'webitel-sdk';
-import endChatSound from '../../../../../public/media/end-chat.wav';
-import newChatSound from '../../../../../public/media/new-chat.wav';
-import newMessageSound from '../../../../../public/media/new-message.wav';
-import ringingSound from '../../../../../public/media/ringing.mp3';
-import notificationIcon from '../../../../../public/notification-icon.png';
+import { CallActions } from 'webitel-sdk';
 import i18n from '../../../../app/locale/i18n';
 
-const NOTIFICATION_VISIBLE_INTERVAL = 2000;
-
-const getNotificationSound = (action) => {
-  switch (action) {
-    case ChatActions.UserInvite:
-      return new Audio(newChatSound);
-    case ChatActions.Message:
-      return new Audio(newMessageSound);
-    case ChatActions.Close:
-      return new Audio(endChatSound);
-    default:
-      return false;
-  }
-};
-
 const getLastMessage = (chat) => chat.messages[chat.messages.length - 1];
-
-const getCurrentTabId = () => localStorage.getItem('currentTabId');
-
-const setCurrentTabId = (value) => localStorage.setItem('currentTabId', value);
-
-const state = {
-  thisTabId: null, // this tab
-  currentTabId: null, // current tab is localStorage
-  broadcastChannel: null, // in order to reset the tab title with unread number
-  unreadCount: 0,
-  currentlyPlaying: false,
-};
-
-const getters = {
-  IS_MAIN_TAB: (state) => state.thisTabId === state.currentTabId,
-  IS_SOUND_ALLOWED: (state, getters) => getters.IS_MAIN_TAB && !state.currentlyPlaying,
-};
 
 const actions = {
   // utils
@@ -51,8 +16,8 @@ const actions = {
       && context.getters.IS_MAIN_TAB) {
       const name = getLastMessage(chat)?.member?.name ||
         chat.messages[0].member.name;
-        const text = i18n.t(`notifications.${snakeToCamel(action)}`, { name });
-        context.dispatch('SEND_NOTIFICATION', { text });
+      const text = i18n.t(`notifications.${snakeToCamel(action)}`, { name });
+      context.dispatch('SEND_NOTIFICATION', { text });
     }
     context.dispatch('INCREMENT_UNREAD_COUNT');
   },
@@ -69,151 +34,13 @@ const actions = {
   },
 
   HANDLE_ANY_CALL_RINGING: (context) => {
-    const sound = new Audio(ringingSound);
-    sound.loop = true;
-    context.dispatch('PLAY_SOUND', { sound });
-  },
-
-  // interface
-  INITIALIZE: (context) => Promise
-    .allSettled([
-                  context.dispatch('_SETUP_THIS_TAB_ID'),
-                  context.dispatch('_SETUP_UNREAD_COUND_BROADCAST_LISTENING'),
-                  context.dispatch('_SUBSCRIBE_TAB_CLOSING'),
-                ]),
-
-  DESTROY: (context) => Promise
-    .allSettled([
-                  context.dispatch('STOP_SOUND'),
-                  context.dispatch('_REMOVE_CURRENT_TAB_ID'),
-                ]),
-
-  PLAY_SOUND: (context, {
-    action,
-    sound = getNotificationSound(action),
-  }) => {
-    if (context.getters.IS_SOUND_ALLOWED
-      && !localStorage.getItem('wtIsPlaying')
-    ) {
-      const audio = sound instanceof Audio ? sound : new Audio(sound);
-      audio.addEventListener('ended', () => {
-        context.dispatch('STOP_SOUND');
-      }, { once: true });
-      audio.play();
-      localStorage.setItem('wtIsPlaying', 'true');
-      context.commit('SET_CURRENTLY_PLAYING', audio);
-    }
-  },
-
-  STOP_SOUND: (context) => {
-    const { currentlyPlaying } = context.state;
-    if (currentlyPlaying) currentlyPlaying.pause();
-    localStorage.removeItem('wtIsPlaying');
-    context.commit('RESET_CURRENTLY_PLAYING');
-  },
-
-  SEND_NOTIFICATION: (context, {
-    locale,
-    text = i18n.t(locale),
-    icon = notificationIcon,
-    interval = NOTIFICATION_VISIBLE_INTERVAL,
-  }) => {
-    const notification = new Notification(text, { icon });
-
-    notification.addEventListener('click', () => {
-      window.focus();
-    }, { once: true });
-
-    setTimeout(() => {
-      notification.close();
-    }, interval);
-  },
-
-  INCREMENT_UNREAD_COUNT: (context) => {
-    const count = context.state.unreadCount + 1;
-    context.dispatch('_SET_UNREAD_COUNT', count);
-    context.state.broadcastChannel.postMessage({ count });
-  },
-
-  // -----
-  _SUBSCRIBE_TAB_CLOSING: (context) => {
-    window.addEventListener('storage', () => {
-      if (!getCurrentTabId()) {
-        setCurrentTabId(context.state.thisTabId);
-      }
-      context.commit('SET_CURRENT_TAB_ID', getCurrentTabId());
-    });
-  },
-
-  _SETUP_THIS_TAB_ID: (context) => {
-    const thisTabId = Math.random().toString();
-    context.commit('SET_THIS_TAB_ID', thisTabId);
-    setCurrentTabId(thisTabId);
-    context.commit('SET_CURRENT_TAB_ID', thisTabId);
-  },
-
-  _SETUP_UNREAD_COUND_BROADCAST_LISTENING: (context) => {
-    const broadcastChannel = new BroadcastChannel('WtAppNotifications');
-    broadcastChannel.addEventListener('message', ({ data }) => {
-      context.dispatch('_SET_UNREAD_COUNT', data.count);
-    });
-
-    document.documentElement.addEventListener('click', () => {
-      context.dispatch('_RESET_UNREAD_COUNT');
-    });
-    context.commit('SET_BROADCAST_CHANNEL', broadcastChannel);
-  },
-
-  _SET_UNREAD_COUNT: (context, count) => {
-    context.commit('SET_UNREAD_COUNT', count);
-    context.dispatch('_SET_TAB_TITLE');
-  },
-
-  _RESET_UNREAD_COUNT: (context) => {
-    if (context.state.unreadCount === 0) return;
-    const count = 0;
-    context.dispatch('_SET_UNREAD_COUNT', count);
-    context.state.broadcastChannel.postMessage({ count });
-  },
-
-  _SET_TAB_TITLE: (context) => {
-    const count = context.state.unreadCount;
-    const titleText = document.title.replace(/\s*\(.*?\)\s*/g, '');
-    document.title = count ? `(${count}) ${titleText}` : titleText;
-  },
-
-  _REMOVE_CURRENT_TAB_ID: () => {
-    localStorage.removeItem('currentTabId');
+    context.dispatch('PLAY_SOUND', { action: CallActions.Ringing });
   },
 };
 
-const mutations = {
-  SET_THIS_TAB_ID: (state, thisTabId) => {
-    state.thisTabId = thisTabId;
-  },
+const notifications = new NotificationsStoreModule()
+  .getModule({
+               actions,
+             });
 
-  SET_CURRENT_TAB_ID: (state, currentTabId) => {
-    state.currentTabId = currentTabId;
-  },
-
-  SET_BROADCAST_CHANNEL: (state, channel) => {
-    state.broadcastChannel = channel;
-  },
-  SET_CURRENTLY_PLAYING: (state, sound) => {
-    state.currentlyPlaying = sound;
-  },
-  RESET_CURRENTLY_PLAYING: (state) => {
-    state.currentlyPlaying = false;
-  },
-  SET_UNREAD_COUNT: (state, count) => {
-    state.unreadCount = count;
-  },
-};
-
-export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations,
-};
+export default notifications;
