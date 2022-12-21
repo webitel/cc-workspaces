03cfa2d58c967e1d616b563a526f405bc1ad718e
Lera24 <bedulinavalera21@gmail.com>
2022-12-21 17:32:29 +0200

    fix: registerWebdevice value[WTEL-3095]

--- src/main.js
+++ src/main.js
@@ -27,7 +27,9 @@ const fetchConfig = async () => {
       });
       const body = await response.json();
       return {
-        CLI: body,
+        CLI: {
+          registerWebDevice: body.webrtc,
+        },
       };
     } catch (error) {
       return {};
