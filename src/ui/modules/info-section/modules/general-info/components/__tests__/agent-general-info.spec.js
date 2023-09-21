5f6c92d548f0f4877ce140a0f6d497aa1f6894c9
dlohvinov <nsblnr@gmail.com>
2023-09-21 15:55:51 +0300

    fix: fixed tests [WTEL-3855]

--- src/ui/modules/info-section/modules/general-info/components/__tests__/agent-general-info.spec.js
+++ src/ui/modules/info-section/modules/general-info/components/__tests__/agent-general-info.spec.js
@@ -1,21 +1,44 @@
 import { shallowMount } from '@vue/test-utils';
+import { createStore } from 'vuex';
+import agentInfo from '../../store/agent-info';
 import GeneralInfoTab from '../general-info-tab.vue';
 
 const agent = {};
 
-const computed = {
-  agent: () => agent,
-  agentInfo: () => ({
-    queues: [],
-    pauseCauses: [],
-    agent,
-  }),
-};
+const store = createStore({
+  modules: {
+    features: {
+      namespaced: true,
+      modules: {
+        status: {
+          state: {
+            agent,
+          },
+          namespaced: true,
+        },
+      },
+    },
+    ui: {
+      namespaced: true,
+      modules: {
+        infoSec: {
+          namespaced: true,
+          modules: {
+            agentInfo,
+          },
+        },
+      },
+    },
+  },
+});
 
 describe('General Info Tab', () => {
   it('renders a component', () => {
-    jest.spyOn(GeneralInfoTab.methods, 'loadAgentInfo').mockImplementation(jest.fn());
-    const wrapper = shallowMount(GeneralInfoTab, { computed });
+    const wrapper = shallowMount(GeneralInfoTab, {
+      global: {
+        plugins: [store],
+      },
+    });
     expect(wrapper.exists()).toBe(true);
   });
 });
