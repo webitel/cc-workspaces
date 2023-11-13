bb8a789fab970d677a5c8d0135906f7aba704b5f
Lera24 <bedulinavalera21@gmail.com>
2023-11-13 18:19:19 +0200

    hotfix: add kz locale[ENPF-58]

--- src/app/locale/i18n.js
+++ src/app/locale/i18n.js
@@ -2,11 +2,13 @@ import { createI18n } from 'vue-i18n';
 import en from './en/en';
 import ru from './ru/ru';
 import ua from './ua/ua';
+import kz from './kz/kz';
 
 const messages = {
   en,
   ru,
   ua,
+  kz,
 };
 
 export default createI18n({
