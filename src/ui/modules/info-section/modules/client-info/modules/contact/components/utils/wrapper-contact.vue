52e7fb194361e8436a3066fb007d7211d14d8f15
Lera24 <bedulinavalera21@gmail.com>
2023-11-27 14:03:16 +0200

    fix: add carrentIndex[WTEL-3711]

--- src/ui/modules/info-section/modules/client-info/modules/contact/components/utils/wrapper-contact.vue
+++ src/ui/modules/info-section/modules/client-info/modules/contact/components/utils/wrapper-contact.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <header-contact
+      :lenght="props.listContacts.length"
       @next="nextContact"
       @prev="prevContact"
     ></header-contact>
@@ -13,7 +14,7 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { useStore } from 'vuex';
 import ContactCard from '../contact-card/the-contact-card.vue';
 import HeaderContact from './header-contact.vue';
@@ -37,17 +38,27 @@ async function setContact(id) {
 
 const contactId = computed(() => store.getters['features/call/CALL_ON_WORKSPACE'].contactId);
 const currentContact = computed(() => store.state.ui.infoSec.client.contact.currentContact);
+const currentIndex = ref(0);
 
-function setCurrentContact(i) {
-  return store.dispatch(`${namespace}/SET_CURRENT_CONTACT`, props.listContacts[i]);
+function setCurrentContact(idx) {
+  return store.dispatch(`${namespace}/SET_CURRENT_CONTACT`, props.listContacts[idx]);
 }
 
+function nextContact() {
+  currentIndex.value += 1;
+  setCurrentContact(currentIndex.value);
+}
+
+function prevContact() {
+  currentIndex.value -= 1;
+  setCurrentContact(currentIndex.value);
+}
 
 onMounted(() => {
   if (props.listContacts.length === 1) {
     setContact(props.listContacts[0].id);
   }
-  setCurrentContact(0);
+  setCurrentContact(currentIndex.value);
 });
 
 </script>
