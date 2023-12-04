d0a81a1c566372889411aa02835acdb551247601
dlohvinov <nsblnr@gmail.com>
2023-12-05 00:48:53 +0200

    feat: appearance store module, dark theme switcher [WTEL-3902]

---
+++ src/ui/modules/appearance/store/appearance.js
@@ -0,0 +1,4 @@
+import AppearanceStoreModule
+  from '@webitel/ui-sdk/src/modules/Appearance/store/AppearanceStoreModule';
+
+export default new AppearanceStoreModule().getModule();
--- src/ui/store/ui.js
+++ src/ui/store/ui.js
@@ -1,9 +1,11 @@
 import ReactiveNowStoreModule from '@webitel/ui-sdk/src/store/ReactiveNowStoreModule/ReactiveNowStoreModule';
 import userinfo from '../modules/userinfo/userinfo';
 import infoSec from '../modules/info-section/store/infoSec';
+import appearance from '../modules/appearance/store/appearance';
 
 const modules = {
   userinfo,
+  appearance,
   now: new ReactiveNowStoreModule().getModule(),
   infoSec,
 };
