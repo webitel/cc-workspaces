a858d47e6e33ffc11ff48290ca122442d966b119
volodymyr <volodymyr.dechtiaruk@gmail.com>
2023-12-20 16:50:55 +0200

    feature: API for new statuses added [WTEL-3798]

--- src/app/api/agent-workspace/endpoints/users/UsersAPIRepository.js
+++ src/app/api/agent-workspace/endpoints/users/UsersAPIRepository.js
@@ -1,14 +1,71 @@
 import { EndpointListGetterApiConsumer } from 'webitel-sdk/esm2015/api-consumers';
 import { objCamelToSnake } from '@webitel/ui-sdk/src/scripts/caseConverters';
+import { AgentServiceApiFactory } from 'webitel-sdk';
+import {
+  getDefaultGetListResponse,
+  getDefaultGetParams,
+} from '@webitel/ui-sdk/src/api/defaults';
+import applyTransform, {
+  merge, mergeEach, notify, snakeToCamel,
+} from '@webitel/ui-sdk/src/api/transformers';
 import instance from '../../../old/instance';
+import instanceNew from '../../../instance';
+import configuration from '../../../openAPIConfig';
 
 const baseUrl = '/users';
 
 const listGetter = new EndpointListGetterApiConsumer({ baseUrl, instance });
+const service = new AgentServiceApiFactory(configuration, '', instanceNew);
 
 const usersAPIRepository = {
   async getUsers(params) {
-    return listGetter.getList(objCamelToSnake(params));
+    console.log(params);
+    const defaultObject = {
+      extension: '',
+      id: '',
+      name: '',
+      presence: [],
+      status: '',
+    };
+
+    const listResponseHandler = (items) => {
+      return items.map((item) => ({
+        ...item,
+      }));
+    };
+
+    const {
+      page,
+      size,
+      search,
+    } = applyTransform(params, [
+      merge(getDefaultGetParams()),
+      snakeToCamel(),
+    ]);
+
+    try {
+      const response = await service.searchUserStatus(
+        page,
+        size,
+        search,
+      );
+      const { items, next } = applyTransform(response.data, [
+        snakeToCamel(),
+        merge(getDefaultGetListResponse()),
+      ]);
+      console.log(response.data);
+      return {
+        items: applyTransform(items, [
+          mergeEach(defaultObject),
+          listResponseHandler,
+        ]),
+        next,
+      };
+    } catch (err) {
+      throw applyTransform(err, [
+        notify,
+      ]);
+    }
   },
   async setUserStatus(status) {
     const url = '/presence';
