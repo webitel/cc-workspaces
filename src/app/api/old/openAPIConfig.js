a21779c93d9a17aee0ae7b03cd8dad00bbb08ce2
lizacoma <my.skin00@gmail.com>
2024-01-24 18:57:27 +0200

    refactor: delete old instance and openAPIConfig files [WTEL-4039]

--- src/app/api/old/openAPIConfig.js
+++
@@ -1,9 +0,0 @@
-import { Configuration } from 'webitel-sdk';
-
-const configuration = new Configuration({
-  basePath: process.env.VUE_APP_API_URL,
-  apiKey: localStorage.getItem('access-key') || '',
-  accessToken: localStorage.getItem('access-key') || '',
-});
-
-export default configuration;
