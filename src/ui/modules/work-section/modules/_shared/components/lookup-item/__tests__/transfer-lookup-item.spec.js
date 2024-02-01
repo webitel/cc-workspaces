50ac011874b510cebf9a2e759b814e7721b1375e
volodymyr <volodymyr.dechtiaruk@gmail.com>
2024-02-01 15:35:56 +0200

    refactor: tests fixed [WTEL-3798]

--- src/ui/modules/work-section/modules/_shared/components/lookup-item/__tests__/transfer-lookup-item.spec.js
+++ src/ui/modules/work-section/modules/_shared/components/lookup-item/__tests__/transfer-lookup-item.spec.js
@@ -56,7 +56,7 @@ describe('TransferLookupItem', () => {
   });
 
   it('correctly comoputes user BUSY status: dlg', () => {
-    item.presence = 'dlg';
+    item.presence = { status: 'dlg' };
     const wrapper = shallowMount(ContactLookupItem, {
       props: { item },
     });
@@ -64,7 +64,7 @@ describe('TransferLookupItem', () => {
   });
 
   it('correctly comoputes user DND status: dnd', () => {
-    item.presence = 'dnd';
+    item.presence = { status: 'dnd' };
     const wrapper = shallowMount(ContactLookupItem, {
       props: { item },
     });
