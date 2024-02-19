54067364607b5b8c7c930597271b896f175b9127
dlohvinov <nsblnr@gmail.com>
2024-02-19 12:21:08 +0200

    tests: added tests for missed calls [WTEL-3899]

--- src/ui/modules/queue-section/modules/call-queue/components/missed-queue/__tests__/missed-queue-preview.spec.js
+++ src/ui/modules/queue-section/modules/call-queue/components/missed-queue/__tests__/missed-queue-preview.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount } from '@vue/test-utils';
 import MissedQueuePreview
   from '../missed-queue-preview.vue';
 
@@ -11,5 +11,57 @@ describe('MissedQueuePreview', () => {
       },
     });
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('emits call event on call button click: MD size', () => {
+    const wrapper = mount(MissedQueuePreview, {
+      props: {
+        task: {},
+        size: 'md',
+      },
+    });
+    wrapper.findComponent({ name: 'wt-rounded-action' }).vm.$emit('click', {
+      preventDefault: () => {}, // .prevent event handler
+    });
+    expect(wrapper.emitted().call).toBeTruthy();
+  });
+
+  it('emits call event on call button click: SM size', () => {
+    const wrapper = mount(MissedQueuePreview, {
+      props: {
+        task: {},
+        size: 'sm',
+      },
+    });
+    wrapper.findComponent({ name: 'wt-rounded-action' }).vm.$emit('click', {
+      preventDefault: () => {}, // .prevent event handler
+    });
+    expect(wrapper.emitted().call).toBeTruthy();
+  });
+
+  it('emits hide event on close icon-button click: MD size', () => {
+    const wrapper = mount(MissedQueuePreview, {
+      props: {
+        task: {},
+        size: 'md',
+      },
+    });
+    wrapper.findComponent('.missed-queue-preview-hide-action').vm.$emit('click', {
+      preventDefault: () => {}, // .prevent event handler
+    });
+    expect(wrapper.emitted().hide).toBeTruthy();
+  });
+
+  it('emits hide event on close icon-button click: SM size', () => {
+    const wrapper = mount(MissedQueuePreview, {
+      props: {
+        task: {},
+        size: 'sm',
+      },
+    });
+    wrapper.findComponent('.missed-queue-preview-hide-action').vm.$emit('click', {
+      preventDefault: () => {}, // .prevent event handler
+    });
+    expect(wrapper.emitted().hide).toBeTruthy();
   });
 });
