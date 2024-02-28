3d34627c817a3f113159ec51359822c77e4f69ea
volodymyr <volodymyr.dechtiaruk@gmail.com>
2024-02-28 12:23:06 +0200

    fix: userStatus computed fixed [WTEL-3798]

--- src/features/modules/agent-status/statusUtils/parseUserStatus.js
+++ src/features/modules/agent-status/statusUtils/parseUserStatus.js
@@ -3,7 +3,7 @@ import UserStatus from './UserStatus';
 const parseUserStatus = (presence) => ({
   [UserStatus.DND]: presence?.status?.includes('dnd'),
   [UserStatus.BUSY]: presence?.status?.includes('dlg'),
-  [UserStatus.WEB]: presence?.status?.includes('sip'),
+  [UserStatus.SIP]: presence?.status?.includes('sip'),
   [UserStatus.WEB]: presence?.status?.includes('web'),
 });
 
