d64df084336563a60d8855670899834e5ee9ac54
Lera24 <bedulinavalera21@gmail.com>
2024-03-04 15:37:02 +0200

    feature: add lookupItemContainer component in containers[WTEL-4234]

--- src/ui/modules/work-section/modules/chat/components/chat-transfer-container/chat-transfer-container.vue
+++ src/ui/modules/work-section/modules/chat/components/chat-transfer-container/chat-transfer-container.vue
@@ -1,37 +1,40 @@
 <template>
-  <div class="ws-worksection chat-transfer-container">
-    <div class="ws-worksection__search-wrap">
-      <wt-search-bar
-        v-model="dataSearch"
-        class="ws-worksection__search"
-        debounce
-        :size="size"
-        @search="resetData"
-      ></wt-search-bar>
+  <div class="chat-transfer-container">
+    <lookup-item-container
+      :empty="!dataList.length"
+      :loading="isLoading"
+      :search="dataSearch"
+      @more="handleIntersect"
+      @search:input="dataSearch = $event"
+      @search:change="resetData"
+    >
+      <template v-slot:search="{ search, inputHandler, searchHandler }">
+        <div class="chat-transfer-container__search-wrap">
+          <wt-search-bar
+            :size="size"
+            :value="search"
+            @input="inputHandler"
+            @search="searchHandler"
+          ></wt-search-bar>
+          <wt-rounded-action
+            :class="{ 'active': transferDestination === TransferDestination.CHATPLAN }"
+            icon="ws-bot"
+            rounded
+            :size="size"
+            @click="transferDestination = TransferDestination.CHATPLAN"
+          ></wt-rounded-action>
+          <wt-icon-btn
+            icon="close"
+            @click="closeTab"
+          ></wt-icon-btn>
+        </div>
+      </template>
 
-<!--      <wt-rounded-action-->
-<!--        :class="{ 'active': transferDestination === TransferDestination.USER }"-->
-<!--        color="secondary"-->
-<!--        icon="ws-agent"-->
-<!--        @click="transferDestination = TransferDestination.USER"-->
-<!--      ></wt-rounded-action>-->
-      <wt-rounded-action
-        :class="{ 'active': transferDestination === TransferDestination.CHATPLAN }"
-        icon="ws-bot"
-        rounded
-        :size="size"
-        @click="transferDestination = TransferDestination.CHATPLAN"
-      ></wt-rounded-action>
-      <wt-icon-btn
-        icon="close"
-        @click="closeTab"
-      ></wt-icon-btn>
-    </div>
+      <template v-slot:empty>
+        <empty-search type="contacts" />
+      </template>
 
-    <section ref="scroll-wrap" class="ws-worksection__list">
-      <wt-loader v-if="isLoading" />
-      <empty-search v-else-if="!dataList.length" :type="'contacts'"></empty-search>
-      <div v-else class="ws-worksection__list-wrap">
+      <template v-slot:content>
         <transfer-lookup-item
           v-for="(item, key) of dataList"
           :id="`scroll-item-${key}`"
@@ -49,13 +52,8 @@
             ></wt-icon>
           </template>
         </transfer-lookup-item>
-      </div>
-
-      <observer
-        :options="obsOptions"
-        @intersect="handleIntersect"
-      />
-    </section>
+      </template>
+    </lookup-item-container>
   </div>
 </template>
 
@@ -63,6 +61,7 @@
 import { mapActions, mapState } from 'vuex';
 import sizeMixin from '../../../../../../../app/mixins/sizeMixin';
 import APIRepository from '../../../../../../../app/api/APIRepository';
+import LookupItemContainer from '../../../_shared/components/lookup-item-container/lookup-item-container.vue';
 import TransferDestination from '../../enums/ChatTransferDestination.enum';
 import infiniteScrollMixin from '../../../../../../../app/mixins/infiniteScrollMixin';
 import EmptySearch from '../../../_shared/components/workspace-empty-search/components/empty-search.vue';
@@ -76,6 +75,7 @@ export default {
   name: 'chat-transfer-container',
   mixins: [infiniteScrollMixin, sizeMixin],
   components: {
+    LookupItemContainer,
     TransferLookupItem,
     EmptySearch,
   },
@@ -131,26 +131,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.ws-worksection__search-wrap {
+.chat-transfer-container {
   display: flex;
-  align-items: center;
-  box-sizing: border-box;
-  width: 100%;
-  margin-bottom: var(--spacing-xs);
-  padding: var(--spacing-xs);
-  background-color: var(--dp-18-surface-color);
-  border-radius: var(--spacing-xs);
+  flex-direction: column;
+  height: 100%;
+  gap: var(--spacing-xs);
 
-  .ws-worksection__search {
-    flex: 1 1 auto;
-    width: auto;
-    min-width: auto;
-    margin: 0;
-  }
-
-  .wt-rounded-action, .wt-icon-btn {
-    flex: 0 0 auto;
-    margin-left: var(--spacing-xs);
+  &__search-wrap {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    gap: var(--spacing-xs);
+    width: 100%;
+    margin-bottom: var(--spacing-xs);
+    padding: var(--spacing-xs);
+    background-color: var(--dp-18-surface-color);
+    border-radius: var(--spacing-xs);
   }
 }
 </style>
