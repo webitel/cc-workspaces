52502b78952952e024f83a7c5ba61604d6e4dd32
volodymyr <volodymyr.dechtiaruk@gmail.com>
2024-03-11 15:00:31 +0200

    fix: websocket endpoint fixed localhost [WTEL-4303]

--- src/app/api/agent-workspace/websocket/WebSocketClientController.js
+++ src/app/api/agent-workspace/websocket/WebSocketClientController.js
@@ -5,7 +5,7 @@ import websocketErrorEventHandler from './websocketErrorEventHandler';
 const { hostname, protocol } = window.location;
 const origin = (`${protocol}//${hostname}`).replace(/^http/, 'ws');
 console.log('origin', origin);
-const endpoint = import.meta.env.NODE_ENV === 'development'
+const endpoint = import.meta.env.NODE_ENV === 'development' || 'localhost'
   ? 'wss://dev.webitel.com/ws' : `${origin}/ws`;
 console.log('endpoint', endpoint);
 const getConfig = () => {
