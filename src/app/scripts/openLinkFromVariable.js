42e310d6b03687b7feb390dc999a93834fae4019
liza-pohranichna <haemoglobin0@gmail.com>
2024-05-27 18:19:23 +0300

    hotfix: added timeout for open link [WTEL-4593]

--- src/app/scripts/openLinkFromVariable.js
+++ src/app/scripts/openLinkFromVariable.js
@@ -4,7 +4,9 @@ import isElectron from 'is-electron';
 const openLinkFromVariable = (task) => {
   if (!task.variables?.link) return false;
   if (isElectron()) return false;
-  return window.open(prependHttp(task.variables.link), '_blank');
+  return setTimeout(() => window.open(prependHttp(task.variables.link), '_blank'),
+    task.variables.link_open_timeout_sec * 1000 || 0)
+  // window.open(prependHttp(task.variables.link), '_blank');
 };
 
 export default openLinkFromVariable;
