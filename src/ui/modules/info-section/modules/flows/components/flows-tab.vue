e3f20ed972930a73b91bd40ccabdcd487ddb8f85
liza-pohranichna <haemoglobin0@gmail.com>
2024-07-11 13:44:53 +0300

    fix: change logic for show flows tab in agent info section [WTEL-4609]

--- src/ui/modules/info-section/modules/flows/components/flows-tab.vue
+++ src/ui/modules/info-section/modules/flows/components/flows-tab.vue
@@ -15,18 +15,12 @@
         <wt-divider />
       </div>
     </ul>
-    <wt-dummy
-      v-else
-      class="flows-tab__dummy"
-      :text="$t('infoSec.flows.dummy')"
-    />
   </section>
 </template>
 
 <script setup>
 import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
-import { useCachedInterval } from '@webitel/ui-sdk/src/composables/useCachedInterval/useCachedInterval';
 import FlowButton from './flow-button.vue';
 import getNamespacedState from '@webitel/ui-sdk/src/store/helpers/getNamespacedState';
 
@@ -35,18 +29,6 @@ const namespace = 'ui/infoSec/flows';
 const store = useStore();
 
 const flowsList = computed(() => getNamespacedState(store.state, namespace).flows);
-
-const { subscribe } = useCachedInterval({ timeout: 5 * 1000 });
-
-async function loadFlowsList() {
-    try {
-     await store.dispatch(`${namespace}/LOAD_FLOWS_LIST`);
-    } catch (err) {
-      throw err;
-    }
-}
-
-subscribe(loadFlowsList);
 
 </script>
 
