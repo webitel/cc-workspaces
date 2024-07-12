13b2916c4eaf71424624c08f670257a91c2d3f0e
liza-pohranichna <haemoglobin0@gmail.com>
2024-07-12 11:21:32 +0300

    fix: fixed tests [WTEL-4609]

--- src/ui/modules/info-section/components/__tests__/info-section.spec.js
+++ src/ui/modules/info-section/components/__tests__/info-section.spec.js
@@ -1,13 +1,33 @@
 import { shallowMount } from '@vue/test-utils';
+import { createStore } from 'vuex';
 import { CallActions } from 'webitel-sdk';
+import WorkspaceStates from '../../../../enums/WorkspaceState.enum.js';
+import InfoSectionModule from '../../store/infoSec.js';
 import InfoSection
   from '../the-agent-info-section.vue';
 
 const callOnWorkspace = { state: CallActions.Active, allowReporting: true };
 
 describe('InfoSection', () => {
+  let state;
+  let store;
+  state = {
+    callOnWorkspace: {
+      variables: {},
+    },
+  };
+  store = createStore({
+    state,
+    modules: {
+      workspace: {
+        state: {},
+        ...InfoSectionModule,
+      },
+    },
+  });
   it('renders a component', () => {
     const wrapper = shallowMount(InfoSection, {
+      global: { plugins: [store] },
       computed: {
         ...InfoSection.computed,
         showProcessing: () => true,
