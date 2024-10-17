581943fd9c30e235c96e19475e83926dda238e05
liza-pohranichna <haemoglobin0@gmail.com>
2024-10-17 22:55:25 +0300

    feature: small refactoring [WTEL-4961]

--- src/app/components/utils/scroll-observer.vue
+++ src/app/components/utils/scroll-observer.vue
@@ -2,59 +2,59 @@
   <div class="observer"></div>
 </template>
 <script>
-// https://vueschool.io/articles/vuejs-tutorials/build-an-infinite-scroll-component-using-intersection-observer-api/
+  // https://vueschool.io/articles/vuejs-tutorials/build-an-infinite-scroll-component-using-intersection-observer-api/
 
-export default {
-  props: {
-    options: Object,
-    root: HTMLElement,
-    rootMargin: {
-      type: String,
-      default: '200px',
-    },
-  },
-  emits: ['intersect'],
-  data: () => ({
-    observer: null,
-  }),
-
-  watch: {
-    options: {
-      handler() {
-        this.setObserver();
+  export default {
+    props: {
+      options: Object,
+      root: HTMLElement,
+      rootMargin: {
+        type: String,
+        default: '200px',
       },
-      immediate: true,
     },
-    root: {
-      handler() {
-        this.setObserver();
+    emits: ['intersect'],
+    data: () => ({
+      observer: null,
+    }),
+
+    watch: {
+      options: {
+        handler() {
+          this.setObserver();
+        },
+        immediate: true,
       },
-      immediate: true,
+      root: {
+        handler() {
+          this.setObserver();
+        },
+        immediate: true,
+      },
     },
-  },
 
-  destroyed() {
-    if (this.observer) this.observer.disconnect();
-  },
-
-  methods: {
-    async setObserver() {
-      await this.$nextTick(); // wait for component to render, so that this.$el which is watched by Observer is available
-
-      if ((this.options || this.root) && !this.observer) { // if parent rendered and we can set root within options
-        this.observer = new IntersectionObserver(([entry]) => {
-          if (entry && entry.isIntersecting) {
-            this.$emit('intersect');
-          }
-        }, {
-          root: this.root,
-          rootMargin: this.rootMargin,
-        });
-        this.observer.observe(this.$el);
-      }
+    destroyed() {
+      if (this.observer) this.observer.disconnect();
     },
-  },
-};
+
+    methods: {
+      async setObserver() {
+        await this.$nextTick(); // wait for component to render, so that this.$el which is watched by Observer is available
+
+        if ((this.options || this.root) && !this.observer) { // if parent rendered and we can set root within options
+          this.observer = new IntersectionObserver(([entry]) => {
+            if (entry && entry.isIntersecting) {
+              this.$emit('intersect');
+            }
+          }, {
+            root: this.root,
+            rootMargin: this.rootMargin,
+          });
+          this.observer.observe(this.$el);
+        }
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
