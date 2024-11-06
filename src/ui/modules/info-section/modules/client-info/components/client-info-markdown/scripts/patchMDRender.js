041fd8e73a35e76f612f3f800e7395ebf2031716
dlohvinov <nsblnr@gmail.com>
2024-11-06 10:48:13 +0200

    fix: rolled back md html minify usage. decided to moved it to backend [WTEL-4472](https://webitel.atlassian.net/browse/WTEL-5342)

--- src/ui/modules/info-section/modules/client-info/components/client-info-markdown/scripts/patchMDRender.js
+++ src/ui/modules/info-section/modules/client-info/components/client-info-markdown/scripts/patchMDRender.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-param-reassign */
-export default (md) => {
-  // Remember old renderer, if overridden, or proxy to default renderer
-  const defaultRender = md.renderer.rules.link_open || ((tokens, idx, options, env, self) => (
-    self.renderToken(tokens, idx, options)
-  ));
+import dompurify from 'dompurify';
 
-  md.renderer.rules.link_open = (tokens, index, options, env, self) => {
+const patchLinkOpenRule = (defaultRule) => {
+  return (tokens, index, options, env, self) => {
     const attrIndex = tokens[index].attrIndex('target');
     if (attrIndex < 0) {
       tokens[index].attrPush(['target', '_blank']); // add new attribute
@@ -13,6 +9,35 @@ export default (md) => {
       // eslint-disable-next-line no-param-reassign
       tokens[index].attrs[attrIndex][1] = '_blank'; // replace value of existing attr
     }
-    return defaultRender(tokens, index, options, env, self); // pass token to default renderer.
+    return defaultRule(tokens, index, options, env, self); // pass token to default renderer.
   };
+};
+
+const patchHtmlBlockRule = (defaultRule) => {
+  return (tokens, index, options, env, self) => {
+    let processedTokens = tokens;
+
+    processedTokens = dompurify.sanitize(processedTokens);
+
+    // Why we do not minify HTML?
+    // https://webitel.atlassian.net/browse/WTEL-5112
+    // https://webitel.atlassian.net/browse/WTEL-4472
+
+    return defaultRule(processedTokens, index, options, env, self); // pass token to default renderer.
+  };
+};
+
+export default (md) => {
+  // Remember old renderer, if overridden, or proxy to default renderer
+  const defaultLinkOpenRule = md.renderer.rules.link_open || ((tokens, idx, options, env, self) => (
+    self.renderToken(tokens, idx, options)
+  ));
+
+  md.renderer.rules.link_open = patchLinkOpenRule(defaultLinkOpenRule);
+
+  const defaultHtmlBlockRule = md.renderer.rules.html_block || ((tokens, idx, options, env, self) => (
+    self.renderToken(tokens, idx, options)
+  ));
+
+  md.renderer.rules.html_block = patchHtmlBlockRule(defaultHtmlBlockRule);
 };
