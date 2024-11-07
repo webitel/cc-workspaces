2c4e07a2133315711fd381b9cdef48cf547dbaf3
dlohvinov <nsblnr@gmail.com>
2024-11-07 13:10:38 +0200

    fix: no ringing on autoanswer [DEV-4714](https://webitel.atlassian.net/browse/DEV-4714)

--- src/features/modules/call/scripts/isIncomingRinging.js
+++ src/features/modules/call/scripts/isIncomingRinging.js
@@ -1,15 +1,28 @@
 import { CallActions, CallDirection } from 'webitel-sdk';
 
+const isPreviewDialer = (call) => call.queue && call.queue.queue_type === 'preview';
+
+const isOutboundPreviewDialer = (call) => (
+  call.direction === CallDirection.Outbound && isPreviewDialer(call)
+);
+
+// https://webitel.atlassian.net/browse/WTEL-3602
+const isOutboundCallWithDisableAutoAnswer = (call) => (
+  // call.direction === CallDirection.Outbound && call.allowAnswer
+  // should i check for autoAnswer? https://webitel.atlassian.net/browse/DEV-4714
+  call.direction === CallDirection.Outbound && call.allowAnswer && !call.params.autoAnswer
+);
+
+const isInboundRinging = (call) => (
+  call.state === CallActions.Ringing && call.direction === CallDirection.Inbound
+);
+
 const isIncomingRinging = (call) => {
-  const isPreviewDialer = call.queue && call.queue.queue_type === 'preview';
-  return call.state === CallActions.Ringing // Inbound ringing
-    && (
-      call.direction === CallDirection.Inbound
-      || (call.direction === CallDirection.Outbound // Outbound preview dialer
-        && isPreviewDialer)
-      || (call.direction === CallDirection.Outbound // Outbound call with disable_auto_answer parameter
-      && call.allowAnswer)
-    );
+  return [
+    isInboundRinging(call),
+    isOutboundPreviewDialer(call),
+    isOutboundCallWithDisableAutoAnswer(call),
+  ].some((condition) => !!condition);
 };
 
 export default isIncomingRinging;
