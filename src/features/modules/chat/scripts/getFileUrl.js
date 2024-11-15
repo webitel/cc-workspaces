b4296b75f2214e050465e5a7128c4270c5346922
liza-pohranichna <haemoglobin0@gmail.com>
2024-11-15 12:05:00 +0200

    fix: added logic for changing file url if file include 'source' in type [WTEL-5360](https://webitel.atlassian.net/browse/WTEL-5360)

--- src/features/modules/chat/scripts/getFileUrl.js
+++ src/features/modules/chat/scripts/getFileUrl.js
@@ -1,5 +1,12 @@
-export default function (fileId) {
+export default function ({ id, type }) {
   const token = localStorage.getItem('access-token');
 
-  return `${import.meta.env.VITE_API_URL}/storage/file/${fileId}/download?access_token=${token}`;
+  let link = `${import.meta.env.VITE_API_URL}/storage/file/${id}/download?access_token=${token}`;
+
+  if (type.includes('source')) {
+    const source = type.match(/source=[^;]+/)[0];
+    link = `${link}&${source}`;
+  }
+
+  return link;
 };
