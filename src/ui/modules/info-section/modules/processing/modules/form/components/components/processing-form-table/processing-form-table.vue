0849b3421fecc6acddc793d814d93e638537a0e4
liza-pohranichna <haemoglobin0@gmail.com>
2025-05-30 16:38:03 +0300

    feature: logic for getting nested value from all objects in table data in progress [WTEL-6890] (https://webitel.atlassian.net/browse/WTEL-6890)

--- src/ui/modules/info-section/modules/processing/modules/form/components/components/processing-form-table/processing-form-table.vue
+++ src/ui/modules/info-section/modules/processing/modules/form/components/components/processing-form-table/processing-form-table.vue
@@ -94,22 +94,20 @@ const isSystemSource = computed(() => props.table?.isSystemSource);
 const systemSourcePath = computed(() => props.table?.systemSource?.path);
 
 const tableColumns = computed(() => {
-
   return props.table?.displayColumns.map((column) => {
-    const fieldArray = column.field.includes(columnsFieldSeparator)
+    const fieldPath = column.field.includes(columnsFieldSeparator) // path to nested value from object (displayed as an array). Example: 'contact.emails.name' will look like ['contact', 'emails', 'name']
       ? column.field.split(columnsFieldSeparator)
       : [column.field];
 
     return {
       ...column,
-      field: fieldArray[0],
-      fieldArray,
+      field: fieldPath[0],
+      fieldPath,
     }
   })
 });
 
 const headers = computed(() => {
-
   return tableColumns.value.map((header) => ({
     ...header,
     text: header.name,
@@ -118,10 +116,63 @@ const headers = computed(() => {
   }));
 });
 
+function getNestedValue(parentValue, path) { // якщо updatedBy:{id: '4', name: 'Софія111111'} і шлях updatedBy.name, то має повернутись updatedBy: 'Софія111111'
+  // метод для заглиблення в об'єкт TODO схоже на скріпт, шоб перевикористовувати десь
+  // TODO сюди трапляє об'єкт/масив поля, в який треба заглибитись та шлях, куди(!) треба заглибитись
+  if (path.length <= 1) return parentValue;
+
+  path = path.slice(1); // first of all delete previous path step
+
+  const key = path[0]; // take key of object for next path step
+
+  if (Array.isArray(parentValue)) {
+    return parentValue.map((object) => (getNestedValue(object[key], path))); // get needed value from every object in array
+  }
+  else if (typeof parentValue === 'object') return getNestedValue(parentValue[key], path);
+
+  else return parentValue;
+
+
+  // ! Корисно: collection[id][prop] = collection[id][prop] || []; https://forum.freecodecamp.org/t/sorting-attributes-inside-js-objects/423653
+
+
+  // трансформувати атемс таким чином, щоб кожен об'єкт в масиві став "плоским"?
+  // і кожен хедер мав тільки те значення, яке вказано в шляху. тобто якщо contact.name.common_name, то тоді це буде { contact: contact.name.common_name }
+  // contact = в залежності від значення того, що лежить в contact.name.common_name
+  // використовувати tableColumns.fieldPath як орієнтир в тому, куди мені треба лізти в об'єкті
+
+}
+
+function handleDataList(dataList) {
+
+  return dataList.map((item) => { // беремо основний масив items
+
+    tableColumns.value.forEach((column) => {
+      const valueOfItem = item[column.field];
+      const path = column.fieldPath.slice(1) // delete from path first step
+
+      if (typeof valueOfItem === 'object' || path.length) return getNestedValue(valueOfItem, path); // Example of path ['contact', 'name']
+      else return item;
+    });
+
+
+  })
+}
+
+// let arrayOfItemProperties = Object.entries(item); // item object to array
+// arrayOfItemProperties = arrayOfItemProperties.map((property) => { // перебір по значенням об'єкту item
+//
+//   const columnOfProperty = tableColumns.value.find((column) => column.field === property); // шукаємо колонку(хедер), який співпадає зі значенням
+//
+//   if (columnOfProperty.fieldPath.length > 1) {
+//     return getNestedValues({ array: property, path: columnOfProperty.fieldPath });
+//   } else return property;
+// });
+// const newItem = Object.fromEntries(arrayOfItemProperties); // return from array to object
 
 async function getDataList() {
 
-const fields = headers.value.map((item) => ( item.value ));
+  const fields = headers.value.map((item) => ( item.value )); // all fields we want to get from API
 
   const { items, next } = await TableApi.getList({
     path: systemSourcePath.value,
@@ -133,13 +184,19 @@ const fields = headers.value.map((item) => ( item.value ));
 }
 
 async function initList() {
+  let data;
+
   if (isSystemSource.value) {
 
     const { items, next } = await getDataList();
-    dataList.value = items;
+    data = items;
     nextAllowed.value = next;
 
-  } else dataList.value = props.table?.source || [];
+  } else data = props.table?.source || [];
+
+  handleDataList(data); // !! не використовується, лише запускається
+
+  dataList.value = data;
 }
 
 async function loadNext() {
