4e7757a03ad97d84d2955bba37bf454d9fc1dd68
liza-pohranichna <haemoglobin0@gmail.com>
2025-08-19 17:26:43 +0300

    fix: enhance link table content component to support single and multiple links [WTEL-7418] (https://webitel.atlassian.net/browse/WTEL-7418)

--- src/ui/modules/info-section/modules/processing/modules/form/components/components/processing-form-table/components/link-table-content.vue
+++ src/ui/modules/info-section/modules/processing/modules/form/components/components/processing-form-table/components/link-table-content.vue
@@ -1,21 +1,49 @@
 <template>
-  <div>
-    <a :href="props.value" target="_blank">
+  <div class="link-table-content">
+    <a v-if="props.value && !isValueArray"
+      :href="props.value"
+       class="link-table-content__link"
+       target="_blank"
+    >
       {{ props.value }}
     </a>
+
+    <ul v-else-if="props.value && isValueArray">
+      <li v-for="(link, index) in props.value"
+          :key="index"
+      >
+        <a :href="link"
+           target="_blank"
+           class="link-table-content__link"
+        >
+          {{ link }}
+        </a>,
+      </li>
+    </ul>
+
+    <span v-else> {{ EMPTY_SYMBOL }} </span>
   </div>
 </template>
 
 <script setup lang="ts">
 
-import { defineProps } from 'vue';
+import { computed, defineProps } from 'vue';
+import { EMPTY_SYMBOL } from '../scripts/tableEmptySymbol';
 
 interface Props {
-  value?: string;
+  value?: string | string[];
 }
 
 const props = withDefaults(defineProps<Props>(), {
   value: '',
 });
 
+const isValueArray = computed(() => Array.isArray(props.value))
+
 </script>
+
+<style scoped>
+.link-table-content__link {
+  color: var(--link-color);
+}
+</style>
