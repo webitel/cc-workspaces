ccc6653467b73e8e3ce72892070901758400f14b
Oles Chorpita <chorpitaoles@gmail.com>
2025-09-25 15:06:48 +0300

    [WTEL-5163] add new widget, update icon for last widget

--- src/app/assets/icons/sprite/index.js
+++ src/app/assets/icons/sprite/index.js
@@ -26,3 +26,6 @@ import './ws-user-description.svg';
 import './ws-client-disconnection.svg';
 import './ws-agent-disconnection.svg';
 import './ws-timeout-disconnection.svg';
+import './ws-widget-all-call-missed.svg';
+import './ws-widget-all-dialer-calls.svg';
+import './ws-widget-call-inbound-queue.svg';
