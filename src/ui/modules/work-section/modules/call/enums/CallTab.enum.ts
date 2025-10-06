46461b89fd80bedb7894ad5db1c369879f5450a2
Oles Chorpita <chorpitaoles@gmail.com>
2025-10-06 18:28:00 +0300

    [WTEL-7669] change enams to object

--- src/ui/modules/work-section/modules/call/enums/CallTab.enum.ts
+++ src/ui/modules/work-section/modules/call/enums/CallTab.enum.ts
@@ -1,9 +1,11 @@
-export const enum CallTab {
-  Numpad = 'numpad',
-  Contacts = 'contacts',
-  History = 'history',
-  Transfer = 'transfer',
-  Bridge = 'bridge',
-}
+export const CallTab = {
+  Numpad: 'numpad',
+  Contacts: 'contacts',
+  History: 'history',
+  Transfer: 'transfer',
+  Bridge: 'bridge',
+} as const;
+
+export type CallTabType = typeof CallTab[keyof typeof CallTab];
 
 export default CallTab;
