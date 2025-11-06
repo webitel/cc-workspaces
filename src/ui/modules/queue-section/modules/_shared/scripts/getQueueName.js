45a2c138be49a36444c6795ea6ae6f32c06c6191
Oles Chorpita <chorpitaoles@gmail.com>
2025-11-06 18:15:11 +0200

    fix: change postion in fuction

--- src/ui/modules/queue-section/modules/_shared/scripts/getQueueName.js
+++ src/ui/modules/queue-section/modules/_shared/scripts/getQueueName.js
@@ -1,5 +1,9 @@
 import { QueueTypeName } from '@webitel/ui-sdk/enums'
 
+export function isTransferQueue (queueName, queueType) {
+  //https://webitel.atlassian.net/browse/WTEL-7762?focusedCommentId=704701
+  return queueName === 'transfer' && queueType === QueueTypeName.NOT_IMPLEMENT;
+}
 export function getQueueName(task) {
   const queueName = task.attempt?.queue?.name || task.queue?.name;
   const queueType = task.queue?.queue_type || task.attempt?.queue?.queue_type;
@@ -10,9 +14,4 @@ export function getQueueName(task) {
   // When transferring a call to a user, skip showing the "transfer" queue name
   // https://webitel.atlassian.net/browse/WTEL-7762
   return '';
-}
-
-export function isTransferQueue (queueName, queueType) {
-  //https://webitel.atlassian.net/browse/WTEL-7762?focusedCommentId=704701
-  return queueName === 'transfer' && queueType === QueueTypeName.NOT_IMPLEMENT;
 }
