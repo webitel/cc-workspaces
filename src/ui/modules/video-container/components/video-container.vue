ae249b2f39760788f9fc39ec75c2bc7f58bd87ea
Oles Chorpita <chorpitaoles@gmail.com>
2025-12-04 17:32:14 +0200

    feat: added galleria [WTEL-7686]

--- src/ui/modules/video-container/components/video-container.vue
+++ src/ui/modules/video-container/components/video-container.vue
@@ -1,5 +1,12 @@
 <template>
   <div v-if="isVideo" class="video-container">
+    <wt-galleria
+      v-model:visible="galleriaVisible"
+      v-model:active-index="galleriaActiveIndex"
+      :value="galleriaData"
+      @download="downloadFile(screenshotData[galleriaActiveIndex].id)"
+      @delete="handleDeleteFromGalleria"
+    />
     <video-call
       v-if="isVideo"
       :sender="call.peerStreams[0]"
@@ -24,12 +31,22 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
 import { VideoCall } from '@webitel/ui-sdk/src/modules/CallSession/index';
 import VideoCallScreenshot from './video-call-screenshot/video-call-screenshot.vue';
+import { WtGalleria } from '@webitel/ui-sdk/components';
+import { FileServicesAPI } from '@webitel/api-services/api';
+import {
+  downloadFile,
+  getScreenRecordingMediaUrl,
+} from '@webitel/api-services/api';
 
 const store = useStore();
+
+const galleriaVisible = ref(false);
+const galleriaActiveIndex = ref(0);
+const screenshotData = ref([]);
 
 const call = computed<any>(
   () => store.getters['features/call/CALL_ON_WORKSPACE'] || {},
@@ -38,12 +55,6 @@ const call = computed<any>(
 const peerStreams = computed<MediaStream[]>(() => call.value.peerStreams || []);
 const localStreams = computed<MediaStream[]>(() => call.value.localStreams || []);
 
-const senderStream = computed<MediaStream | undefined>(
-  () => peerStreams.value[0]
-);
-const receiverStream = computed<MediaStream | undefined>(
-  () => localStreams.value[0],
-);
 
 const isPeerVideo = computed(() =>
   peerStreams.value.some((stream) =>
@@ -68,6 +79,16 @@ const screenshotIsLoading = computed<boolean>(
 const screenshotPreviewUrl = computed<string | null>(
   () => store.getters['features/call/videoCall/SCREENSHOT_PREVIEW_URL'],
 );
+const agentId = computed(() => store.state?.features?.status?.agent?.agentId);
+
+const galleriaData = computed(() => {
+  return screenshotData.value?.map((item) => ({
+    src: getScreenRecordingMediaUrl(item.id, false),
+    thumbnailSrc: getScreenRecordingMediaUrl(item.id, true),
+    title: item.view_name,
+    alt: item.view_name,
+  }));
+});
 
 const onToggleRecordings = (e) =>
   store.dispatch('features/call/videoCall/TOGGLE_RECORDINGS', e);
@@ -77,10 +98,34 @@ const onScreenshot = (e) => store.dispatch('features/call/videoCall/MAKE_SCREENS
 const onCloseScreenshot = () =>
   store.dispatch('features/call/videoCall/CLOSE_SCREENSHOT');
 
-const onZoomScreenshot = () => {
-  console.log('zoom screenshot', screenshotPreviewUrl.value);
+const onZoomScreenshot = async () => {
+  await getScreenshots()
+  galleriaVisible.value = true;
+};
+const getScreenshots = async () => FileServicesAPI.getListByCall(call.value.id)
+  .then(res => screenshotData.value = res.items)
+
+
+
+const handleDeleteFromGalleria = () => {
+  handleDelete([screenshotData.value[galleriaActiveIndex.value]]);
+  if (galleriaActiveIndex.value > 0) galleriaActiveIndex.value -= 1;
 };
 
+const handleDelete = async (items: []) => {
+  const deleteEl = (el) => {
+    return FileServicesAPI.deleteScreenRecordingsByAgent({
+      id: el.id,
+      agentId: agentId,
+    });
+  };
+
+  try {
+    await Promise.all(items.map(deleteEl));
+  } finally {
+    await getScreenshots();
+  }
+};
 </script>
 <style scoped lang="scss">
 .video-container {
