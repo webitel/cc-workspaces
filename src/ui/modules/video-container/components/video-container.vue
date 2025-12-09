a5a3abc34e79a1abae1e9b98f07711498e3e4e94
Oles Chorpita <chorpitaoles@gmail.com>
2025-12-09 17:34:25 +0200

    feat: fixed import typed call and actions calls [WTEL-7682]

--- src/ui/modules/video-container/components/video-container.vue
+++ src/ui/modules/video-container/components/video-container.vue
@@ -18,9 +18,7 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 import { useStore } from 'vuex';
-import { VideoCall } from '@webitel/ui-sdk/modules/CallSession';
-import { VideoCallAction } from '@webitel/ui-sdk/modules/CallSession';
-
+import { VideoCall, VideoCallAction } from '@webitel/ui-sdk/modules/CallSession';
 const store = useStore();
 
 const videoCallActions = [
