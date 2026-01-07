55d17913d8987a818a01d5fb6c081cc451b91701
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-01-07 18:35:35 +0200

    fix: remove try catch block (https://webitel.atlassian.net/browse/WTEL-8582) [WTEL-8582]

--- src/ui/modules/info-section/modules/processing/script/getUserTimezone.js
+++ src/ui/modules/info-section/modules/processing/script/getUserTimezone.js
@@ -1,12 +1,8 @@
 import { TIMEZONE_STORAGE_KEY } from '@webitel/ui-sdk/src/modules/Userinfo/v2/constants/UserSettingsConstants';
 
 export function getUserTimezone() {
-  try {
-    const storedTimezone = localStorage.getItem(TIMEZONE_STORAGE_KEY);
-    if (storedTimezone) return storedTimezone;
-  } catch {
-    // Ignore storage access errors and fall back to browser timezone.
-  }
+  const storedTimezone = localStorage.getItem(TIMEZONE_STORAGE_KEY);
+  if (storedTimezone) return storedTimezone;
 
   return Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
