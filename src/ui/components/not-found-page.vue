88bd01f647d1a1aefb58ae6556267041b4374eca
Oles Chorpita <chorpitaoles@gmail.com>
2026-01-09 16:36:57 +0200

    fix: show 404 when not correct url [WTEL-8142]

---
+++ src/ui/components/not-found-page.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="main-not-found-page">
+    <wt-notifications-bar />
+    <cc-header />
+    <wt-error-page type="404" @back="goToHome" />
+  </main>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+import CcHeader from '../modules/app-header/components/app-header.vue';
+
+const router = useRouter();
+
+const goToHome = () => {
+  router.push('/');
+};
+</script>
+
