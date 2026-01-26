37dbb5505d3c0633a8dcfab00dab94bb219d2470
liza-pohranichna <haemoglobin0@gmail.com>
2026-01-26 19:32:18 +0200

    fix: handle chat actions based on video call state and improve message handling [WTEL-8502] (https://webitel.atlassian.net/browse/WTEL-8502)

--- src/ui/modules/work-section/modules/call/module/video-call/module/chat/components/the-video-call-chat.vue
+++ src/ui/modules/work-section/modules/call/module/video-call/module/chat/components/the-video-call-chat.vue
@@ -4,12 +4,7 @@
     <chat-container
       :messages="messages"
       :size="props.size"
-      :chat-actions="[
-        ChatAction.SendMessage,
-        ChatAction.AttachFiles,
-        ChatAction.EmojiPicker,
-        ChatAction.QuickReplies
-        ]"
+      :chat-actions="chatActions"
       @[`action:${ChatAction.SendMessage}`]="sendMessage"
       @[`action:${ChatAction.AttachFiles}`]="sendFiles"
       @[MessageAction.ClickOnImage]="openMedia"
@@ -21,15 +16,21 @@
 import { applyTransform,
   notify,
 } from '@webitel/api-services/api/transformers';
-import { ChatAction, ChatContainer, MessageAction } from '@webitel/ui-chats/ui';
+import { ChatAction, ChatContainer, ChatMessageType, MessageAction } from '@webitel/ui-chats/ui';
 import { ComponentSize } from '@webitel/ui-sdk/enums';
 import type { ResultCallbacks } from '@webitel/ui-sdk/src/types';
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, watch, ref } from 'vue';
 import { useStore } from 'vuex';
 
 import MediaViewer from '../../../../../../chat/media-viewer/media-viewer.vue';
+import { ConversationState } from 'webitel-sdk';
 
 const chatNamespace = 'features/chat';
+const actionsForOpenedChat = [
+  ChatAction.AttachFiles,
+  ChatAction.EmojiPicker,
+  ChatAction.SendMessage,
+];
 
 const props = withDefaults(
   defineProps<{
@@ -42,13 +43,15 @@ const props = withDefaults(
 
 const store = useStore();
 
+const chatActions = ref(actionsForOpenedChat);
+
 const chat = computed(() =>
   store.getters['features/call/videoCall/chat/VIDEO_CALL_CHAT']
 );
-
 const messages = computed(() =>
   store.getters['features/call/videoCall/chat/VIDEO_CALL_CHAT_MESSAGES']
 );
+const isChatClosed = computed(() => chat?.value.state === ConversationState.Closed);
 
 async function sendMessage(text: string, options?: ResultCallbacks) {
   try {
@@ -65,8 +68,8 @@ async function sendMessage(text: string, options?: ResultCallbacks) {
 async function sendFiles(files: File[], options?: ResultCallbacks) {
   try {
     Array.isArray(files)
-      ? await Promise.all(files.map((file) => chat.value?.sendFile(file)))
-      : await chat.value.sendFile(files);
+      ? await Promise.all(files?.map((file) => chat.value?.sendFile(file)))
+      : await chat.value?.sendFile(files);
   } catch (error) {
     throw applyTransform(error, [
       notify,
@@ -76,7 +79,7 @@ async function sendFiles(files: File[], options?: ResultCallbacks) {
   }
 }
 
-const openMedia = (message) => {
+const openMedia = (message: ChatMessageType) => {
   store.dispatch(`${chatNamespace}/chatMedia/OPEN_MEDIA`, message)
 };
 
@@ -87,6 +90,11 @@ onMounted(() => {
   // https://webitel.atlassian.net/browse/WTEL-7689
   chat.value?.join()
 });
+
+watch(isChatClosed, (value:boolean) => {
+  if (value) chatActions.value = [];
+  if (!value && chatActions.value) chatActions.value = actionsForOpenedChat;
+})
 
 </script>
 <style scoped>
