1f22d499a569b9d2cb6eae23596022e97bfab6a9
liza-pohranichna <haemoglobin0@gmail.com>
2026-02-11 23:42:34 +0200

    feature: integrate task header expansion card into chat header component [WTEL-8877] (https://webitel.atlassian.net/browse/WTEL-8877)

--- src/ui/modules/work-section/modules/_shared/components/task-header-expansion-card/task-header-expansion-card.vue
+++ src/ui/modules/work-section/modules/_shared/components/task-header-expansion-card/task-header-expansion-card.vue
@@ -1,17 +1,17 @@
 <template>
   <wt-expansion-card class="task-header-expansion-card">
     <template #title>
-      <wt-avatar :username="username"/>
+      <wt-avatar :username="props.username"/>
       <a
-        v-if="props.contact?.id"
+        v-if="props.contactId"
         :href="contactLink"
         class="task-header-expansion-card__title"
         target="_blank"
       >
-        {{ props.contact.name }}
+        {{ props.username }}
       </a>
       <span v-else>
-          {{ props.username }}
+        {{ props.username }}
       </span>
     </template>
     <p>{{ props.phoneNumber }}</p>
@@ -26,31 +26,37 @@
 import { computed } from 'vue';
 import { useStore } from 'vuex';
 
-import WtExpansionCard from '@webitel/ui-sdk/components/wt-expansion-card/wt-expansion-card'
+import WtExpansionCard from '@webitel/ui-sdk/components/wt-expansion-card/wt-expansion-card';
 import QueueNameChip from '../queue-name-chip/queue-name-chip.vue';
 
-const store  = useStore();
+const store = useStore();
 
 const props = withDefaults(
-  defineProps<{
-    username?: string;
-    phoneNumber?: string;
-    queueName?: string;
-    contact: {
-      id?: string;
-      name?: string;
-    }
-  }>(),
-  {
-    username: '',
-    phoneNumber: '',
-    contact: {},
-  });
-const username = computed(() => props.contact.name || props.username);
-
-const contactLink = computed(() =>
-  store.getters['ui/infoSec/client/contact/CONTACT_LINK'](props.contact.id)
+	defineProps<{
+		username: string;
+		phoneNumber?: string;
+		queueName?: string;
+		contactId?: string;
+	}>(),
+	{
+		queueName: '',
+		phoneNumber: '',
+		contactId: '',
+	},
 );
 
+const contactLink = computed(() =>
+	store.getters['ui/infoSec/client/contact/CONTACT_LINK'](props.contactId),
+);
 </script>
+
+<style>
+  .task-header-expansion-card__title {
+    color: var(--text-main-color);
+  }
+
+  .task-header-expansion-card__title:hover {
+    text-decoration: underline;
+  }
+</style>
 
--- src/ui/modules/work-section/modules/chat/chat-header/chat-header.vue
+++ src/ui/modules/work-section/modules/chat/chat-header/chat-header.vue
@@ -1,7 +1,5 @@
 <template>
-  <task-header
-    :size="size"
-  >
+  <task-header :size="props.size">
     <template #end-section>
       <wt-rounded-action
         v-show="isTransferAction"
@@ -19,131 +17,104 @@
       />
     </template>
     <template #info>
-      <a
-        v-if="chatContact?.id"
-        :href="contactLink(chatContact.id)"
-        class="chat-header-title"
-        target="_blank"
-      >
-        {{ chatContact.name }}
-      </a>
-      <span v-else>
-        {{ displayChatName }}
-      </span>
+      <task-header-expansion-card
+        :username="displayChatName"
+        :phone-number="displayNumber"
+        :contactId="props.chatContact?.id"
+        :queue-name="displayQueueName"
+      />
     </template>
-
-    <queue-name-chip
-      v-if="displayQueueName"
-      :name="displayQueueName"
-    />
   </task-header>
 </template>
 
 <script lang="ts" setup>
 import { ComponentSize } from '@webitel/ui-sdk/enums';
-import { computed, onMounted, onUnmounted } from 'vue';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
 import { useStore } from 'vuex';
 
 import HotkeyAction from '../../../../../hotkeys/HotkeysActiom.enum';
 import { useHotkeys } from '../../../../../hotkeys/useHotkeys';
 import { getQueueName } from '../../../../../modules/queue-section/modules/_shared/scripts/getQueueName';
 
-import QueueNameChip from '../../_shared/components/queue-name-chip/queue-name-chip.vue';
 import TaskHeader from '../../_shared/components/task-header/task-header.vue';
 import ChatHeaderCloseAction from './chat-header-close-action.vue';
+import TaskHeaderExpansionCard from '../../_shared/components/task-header-expansion-card/task-header-expansion-card.vue';
 
 interface ChatContact {
-  id?: string | number;
-  name?: string;
-}
+	id?: string | number;
+	name?: string;
+} // винести в окремий файл
 
 const props = withDefaults(
-  defineProps<{
-    size?: ComponentSize;
-    chatContact?: ChatContact;
-  }>(),
-  {
-    size: ComponentSize.MD,
-    chatContact: {},
-  },
+	defineProps<{
+		size?: ComponentSize;
+		chatContact?: ChatContact;
+	}>(),
+	{
+		size: ComponentSize.MD,
+		chatContact: {},
+	},
 );
 
 const emit = defineEmits<{
-  openTab: [string];
+	openTab: [
+		string,
+	];
 }>();
-
 
 const store = useStore();
 
-const chat = computed(() =>
-  store.getters['features/chat/CHAT_ON_WORKSPACE'],
-);
+const hotkeyUnsubscribers = ref([]);
 
-const isCloseAction = computed(() =>
-  store.getters['features/chat/ALLOW_CHAT_CLOSE'],
+const chat = computed(() => store.getters['features/chat/CHAT_ON_WORKSPACE']);
+const isCloseAction = computed(
+	() => store.getters['features/chat/ALLOW_CHAT_CLOSE'],
 );
-
-const isTransferAction = computed(() =>
-  store.getters['features/chat/ALLOW_CHAT_TRANSFER'],
+const isTransferAction = computed(
+	() => store.getters['features/chat/ALLOW_CHAT_TRANSFER'],
 );
-
-const contactLink = computed(() =>
-  store.getters['ui/infoSec/client/contact/CONTACT_LINK'],
-);
-
+// const contactLink = computed(() =>
+//   store.getters['ui/infoSec/client/contact/CONTACT_LINK'],
+// );
 const displayChatName = computed(() => {
-  const currentChat = chat.value;
+	const currentChat = chat.value;
 
-  if (props.chatContact?.id) return props.chatContact?.name;
+	if (props.chatContact?.id) return props.chatContact?.name;
+	if (currentChat?.members?.length) {
+		return currentChat.members.map((member: any) => member.name).join(', ');
+	}
+	if (currentChat?.title) return currentChat.title;
 
-  if (currentChat?.members?.length) {
-    return currentChat.members.map((member: any) => member.name).join(', ');
-  }
-
-  if (currentChat?.title) return currentChat.title;
-
-  return 'unknown';
+	return 'unknown';
 });
 
-const displayName = computed(() => chat.value?.displayName);
+// const displayName = computed(() => chat.value?.displayName);
 const displayNumber = computed(() => chat.value?.displayNumber);
-const displayQueueName = computed(() => getQueueName(chat.value),);
-
+const displayQueueName = computed(() => getQueueName(chat.value));
 
 const close = () => store.dispatch('features/chat/CLOSE');
-const openTransferTab = () => { emit('openTab', 'transfer');};
-
-let hotkeyUnsubscribers: Array<() => void> = []; ///??????
+const openTransferTab = () => {
+	emit('openTab', 'transfer');
+};
 
 const setupHotkeys = () => {
-  hotkeyUnsubscribers = useHotkeys([
-    {
-      event: HotkeyAction.END,
-      callback: close,
-    },
-    {
-      event: HotkeyAction.TRANSFER,
-      callback: () => {
-        if (isTransferAction.value) openTransferTab();
-      },
-    },
-  ]);
+	hotkeyUnsubscribers.value = useHotkeys([
+		{
+			event: HotkeyAction.END,
+			callback: close,
+		},
+		{
+			event: HotkeyAction.TRANSFER,
+			callback: () => {
+				if (isTransferAction.value) openTransferTab();
+			},
+		},
+	]);
 };
 
 onMounted(setupHotkeys);
 
 onUnmounted(() => {
-  hotkeyUnsubscribers.forEach((unsubscribe) => unsubscribe());
+	hotkeyUnsubscribers.value?.forEach((unsubscribe) => unsubscribe());
 });
 </script>
-
-
-<style scoped>
-.chat-header-title {
-  color: var(--text-main-color);
-}
-
-a:hover {
-  text-decoration: underline;
-}
-</style>
