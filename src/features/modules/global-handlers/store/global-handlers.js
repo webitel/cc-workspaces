26f6ca0ddbc0277db5f8dd9d185ea31e8c9fdc92
Oles Chorpita <chorpitaoles@gmail.com>
2026-02-16 10:35:49 +0200

    fix: fixed clear task data when socket disconnected [WTEL-8920]

--- src/features/modules/global-handlers/store/global-handlers.js
+++ src/features/modules/global-handlers/store/global-handlers.js
@@ -15,7 +15,10 @@ const actions = {
 	},
 	SUBSCRIBE_TO_CLIENT_DISCONNECT: async (context) => {
 		const client = await context.rootState.client.getCliInstance();
-		client.on('disconnected', () => context.dispatch('OPEN_DISCONNECT_POPUP'));
+		client.on('disconnected', () => {
+			context.dispatch('CLEAR_ALL_TASKS_ON_DISCONNECT');
+			context.dispatch('OPEN_DISCONNECT_POPUP');
+		});
 	},
 	SUBSCRIBE_TO_PHONE_REGISTRATION: async (context) => {
 		const client = await context.rootState.client.getCliInstance();
@@ -30,6 +33,20 @@ const actions = {
 		context.commit('SET_DISCONNECT_POPUP', true),
 	CLOSE_DISCONNECT_POPUP: (context) =>
 		context.commit('SET_DISCONNECT_POPUP', false),
+	/**
+	 * @author o.chorpita
+	 * clears all active tasks (calls, chats, jobs) from store when connection is lost.
+	 * https://webitel.atlassian.net/browse/WTEL-8920
+	 */
+	CLEAR_ALL_TASKS_ON_DISCONNECT: (context) => {
+		// This prevents getters like CALL_ON_WORKSPACE from returning stale tasks
+		context.commit('workspace/SET_STATE_HISTORY', [], { root: true });
+		
+		context.dispatch('features/call/SET_CALL_LIST', [], { root: true });
+		context.commit('features/call/CLEAR_CALL_INFO', null, { root: true });
+		context.dispatch('features/chat/SET_CHAT_LIST', [], { root: true });
+		context.commit('features/job/SET_JOB_LIST', [], { root: true });
+	},
 };
 
 const mutations = {
