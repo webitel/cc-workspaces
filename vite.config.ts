c180128418d5fc12b1f1aef55582c3b2fe1bba3f
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-02-23 12:58:44 +0200

    chore: remove define block from vite-config [WTEL-8897](https://webitel.atlassian.net/browse/WTEL-8897)

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import vue from '@vitejs/plugin-vue';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { VitePWA } from 'vite-plugin-pwa';
 import vueDevtools from 'vite-plugin-vue-devtools';
 import { resolve } from 'path';
 
 export default ({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
-
 	return defineConfig({
 		base: '/workspace',
-		define: {
-			'process.env': JSON.parse(
-				JSON.stringify(env).replaceAll('VITE_', 'VUE_APP_'),
-			),
-		},
 		server: {
 			// host: true,  // uncomment me to enable localhost access by IP (including from other devices in the network)
 		},
@@ -52,8 +45,8 @@ export default ({ mode }) => {
 					compilerOptions: {
 						compatConfig: {
 							MODE: 2,
-              // avoid Vue2 compat v-model warnings
-              COMPONENT_V_MODEL: false,
+							// avoid Vue2 compat v-model warnings
+							COMPONENT_V_MODEL: false,
 						},
 					},
 				},
