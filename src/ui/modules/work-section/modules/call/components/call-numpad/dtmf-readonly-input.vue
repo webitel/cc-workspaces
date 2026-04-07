8558620650c081abc44a941b569aecbd89d97661
liza-pohranichna <haemoglobin0@gmail.com>
2026-04-07 22:33:10 +0300

    feat: add readonly DTMF input component for displaying call state [WTEL-9051] (https://webitel.atlassian.net/browse/WTEL-9051)

---
+++ src/ui/modules/work-section/modules/call/components/call-numpad/dtmf-readonly-input.vue
@@ -0,0 +1,16 @@
+<template>
+  <wt-input-text
+    :model-value="dtmfValue"
+    readonly
+  />
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useCallState } from '../../../../../../composables/useCallState.ts';
+
+const { dtmf } = useCallState();
+const dtmfValue = computed(() =>
+	dtmf.value?.length > 0 ? dtmf.value?.join('') : '',
+);
+</script>
