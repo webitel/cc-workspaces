118dab0200d8fa7e9918a6aadc43e4774fef1a61
liza-pohranichna <haemoglobin0@gmail.com>
2026-04-10 20:04:52 +0300

    refactor: optimize dtmf value computation in readonly input component [WTEL-9051] (https://webitel.atlassian.net/browse/WTEL-9051)

--- src/ui/modules/work-section/modules/call/components/call-numpad/dtmf-readonly-input.vue
+++ src/ui/modules/work-section/modules/call/components/call-numpad/dtmf-readonly-input.vue
@@ -11,6 +11,6 @@ import { useCallState } from '../../../../../../composables/useCallState.ts';
 
 const { dtmf } = useCallState();
 const dtmfValue = computed(() =>
-	dtmf.value?.length > 0 ? dtmf.value?.join('') : '',
+	dtmf.value?.length ? dtmf.value?.join('') : '',
 );
 </script>
