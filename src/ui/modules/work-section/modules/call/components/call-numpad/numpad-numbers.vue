68ecfc0958b98378f0e9aea4872143192b76481d
liza-pohranichna <haemoglobin0@gmail.com>
2026-04-15 21:51:06 +0300

    refactor: streamline numpad component and improve button handling [WTEL-9051] (https://webitel.atlassian.net/browse/WTEL-9051)

--- src/ui/modules/work-section/modules/call/components/call-numpad/numpad-numbers.vue
+++ src/ui/modules/work-section/modules/call/components/call-numpad/numpad-numbers.vue
@@ -3,46 +3,39 @@
     <wt-button
       v-for="(value) of ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#', '+']"
       :key="value"
-      :size="size"
-      class="numpad-numbers__num"
+      :size="ComponentSize.MD"
+      class="numpad-numbers__number"
       color="secondary"
       outline
       rounded
-      @click="input(value)"
+      @click="inputNumber(value)"
     >{{ value }}
     </wt-button>
   </div>
 </template>
 
-<script>
-import sizeMixin from '../../../../../../../app/mixins/sizeMixin';
+<script setup>
+import { ComponentSize } from '@webitel/ui-sdk/enums';
+import { WtButton } from '@webitel/ui-sdk/src/components';
 
-export default {
-	name: 'NumpadNumbers',
-	mixins: [
-		sizeMixin,
-	],
-	methods: {
-		input(value) {
-			this.$emit('input', value);
-		},
-	},
-};
+const emit = defineEmits([
+	'input',
+]);
+
+function inputNumber(value) {
+	emit('input', value);
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .numpad-numbers {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-gap: var(--spacing-xs) var(--spacing-sm);
+  grid-gap: var(--spacing-2xs);
   width: auto;
+}
 
-  :last-child {
-    grid-column-start: 2;
-  }
-
-  @media screen and (max-height: 900px) {
-    grid-gap: var(--spacing-xs);
-  }
+.numpad-numbers__number:last-child {
+  grid-column-start: 2;
 }
 </style>
