4cbaa8577d8e2cd2ec4e5c3d21673193cca05573
Олександр Пальонний <s2567k@gmail.com>
2026-05-01 15:40:03 +0300

    fix: set return for condition [WTEL-9389](https://webitel.atlassian.net/browse/WTEL-9389)

--- src/ui/modules/queue-section/modules/call-queue/components/active-queue/active-queue-preview.vue
+++ src/ui/modules/queue-section/modules/call-queue/components/active-queue/active-queue-preview.vue
@@ -187,7 +187,7 @@ export default {
 		},
 
 		displayNumber() {
-			if (this.task.hideNumber) this.task.destination;
+			if (this.task.hideNumber) return this.task.destination;
 
 			//https://webitel.atlassian.net/browse/WTEL-8215
 			return this.normalizePhoneNumber(this.task.displayNumber);
