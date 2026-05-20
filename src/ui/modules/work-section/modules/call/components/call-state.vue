7743e6ab6533dd14d3e73f0a039b665c5156acac
Lera24 <bedulinavalera21@gmail.com>
2026-05-20 18:05:47 +0300

    fix: display dtmf value[WTEL-9559](https://webitel.atlassian.net/browse/WTEL-9559)

--- src/ui/modules/work-section/modules/call/components/call-state.vue
+++ src/ui/modules/work-section/modules/call/components/call-state.vue
@@ -27,6 +27,12 @@
         {{ digit }}
       </span>
     </div>
+    <div
+      v-if="dtmf"
+      class="call-state__secondary-text typo-subtitle-1"
+    >
+      {{ dtmf.join('') }}
+    </div>
   </div>
 </template>
 
@@ -39,7 +45,7 @@ import { useCallState } from '../../../../../composables/useCallState';
 defineOptions({
 	name: 'CallState',
 });
-const { callState, showTimer, displayTime, sonarIcon } = useCallState();
+const { dtmf, callState, showTimer, displayTime, sonarIcon } = useCallState();
 </script>
 
 <style
