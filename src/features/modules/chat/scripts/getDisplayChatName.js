f123561807ed31b290a3546b8e222d603379e088
Lera24 <bedulinavalera21@gmail.com>
2026-05-21 13:48:50 +0300

    fix: display agent name in chats[WTEL-9570](https://webitel.atlassian.net/browse/WTEL-9570)

--- src/features/modules/chat/scripts/getDisplayChatName.js
+++ src/features/modules/chat/scripts/getDisplayChatName.js
@@ -1,11 +1,18 @@
 export default function getDisplayChatName({ chat, contact, userId }) {
 	// @author @Lera24
 	// https://webitel.atlassian.net/browse/WTEL-9570?focusedCommentId=755185
-	// we don't show the current user
-	const filteredMembers =
-		chat?.members?.filter(
-			(member) => Number(member?.user_id) !== Number(userId),
-		) ?? [];
+
+	const filteredMembers = chat?.members
+		? [
+				...chat.members,
+			]
+		: [];
+	// we don't show the current user if the chat is without a transfer
+	// and show all subsequent times if there was a transfer.
+	const firstMatchIndex = filteredMembers.findIndex(
+		(member) => Number(member?.user_id) === Number(userId),
+	);
+	if (firstMatchIndex > -1) filteredMembers.splice(firstMatchIndex, 1);
 
 	const memberNames = filteredMembers.map((member) => member?.name);
 
