16216341daad4918a10733f6777959641e2631d7
lizacoma <my.skin00@gmail.com>
2026-06-29 22:42:23 +0300

    fix: rewrite user-lookup-item.vue to composition API, add UserLookupItem type and return prev code in transfer-lookup-item.vue [WTEL-6448](https://webitel.atlassian.net/browse/WTEL-6448)

--- src/ui/modules/work-section/modules/_shared/components/lookup-item/user-lookup-item.vue
+++ src/ui/modules/work-section/modules/_shared/components/lookup-item/user-lookup-item.vue
@@ -2,11 +2,11 @@
   <lookup-item>
     <template #before>
       <wt-avatar
-        :status="userStatus"
-        :size="size"
-        :username="item.name"
-        badge
-      ></wt-avatar>
+          :status="userStatus"
+          :size="size"
+          :username="item.name"
+          badge
+      />
     </template>
 
     <template #title>
@@ -19,69 +19,90 @@
 
     <template #after>
       <wt-rounded-action
-        :size="size"
-        :loading="loading"
-        icon="call--filled"
-        color="success"
-        rounded
-        @click="handleInput"
-      ></wt-rounded-action>
+          :size="props.size"
+          :loading="props.loading"
+          icon="call--filled"
+          color="success"
+          rounded
+          @click="handleInput"
+      />
     </template>
   </lookup-item>
 </template>
 
-<script>
+<script setup lang="ts">
+import { computed, withDefaults, defineProps, defineEmits } from 'vue';
+
 import AbstractUserStatus from '@webitel/ui-sdk/src/enums/AbstractUserStatus/AbstractUserStatus.enum';
 import AgentStatus from '@webitel/ui-sdk/src/enums/AgentStatus/AgentStatus.enum';
 
-import sizeMixin from '../../../../../../../app/mixins/sizeMixin';
 import parseUserStatus from '../../../../../../../features/modules/agent-status/statusUtils/parseUserStatus';
 import UserStatus from '../../../../../../../features/modules/agent-status/statusUtils/UserStatus';
-import lookupItemMixin from './mixins/lookupItemMixin';
+import { UserLookupItem } from './types/UserLookupItem'
+import LookupItem from './lookup-item.vue';
 
-export default {
-	name: 'UserLookupItem',
-	mixins: [
-		lookupItemMixin,
-		sizeMixin,
-	],
-	emits: [
-		'input',
-	],
-	props: {
-		loading: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	computed: {
-		// NOTE: this computed is needed to return user status by priority because user can have several statuses. See this task https://my.webitel.com/browse/WTEL-3798
-		userStatus() {
-			const status = parseUserStatus(this.item.presence);
-			if (status[UserStatus.DND]) return AbstractUserStatus.DND;
-			if (status[UserStatus.BUSY]) return AbstractUserStatus.BUSY;
-			if (this.item.status === AgentStatus.ONLINE)
-				return AbstractUserStatus.ONLINE;
-			if (
-				this.item.status === AgentStatus.PAUSE ||
-				this.item.status === AgentStatus.BREAK_OUT
-			)
-				return AbstractUserStatus.PAUSE;
-			if (status[UserStatus.SIP] || status[UserStatus.WEB])
-				return AbstractUserStatus.ACTIVE;
-			return AbstractUserStatus.OFFLINE;
-		},
-	},
-	methods: {
-		handleInput() {
-			if (this.loading) return;
+interface UserLookupItemProps {
+  item: any;
+  loading?: boolean;
+  size?: string;
+}
 
-			this.$emit('input', this.item);
-		},
-	},
+interface UserLookupItemEmits {
+
+}
+
+const props = withDefaults(
+    defineProps<{
+      item: UserLookupItem;
+      loading?: boolean;
+      size?: string;
+    }>(), {
+      loading: false,
+      size: '',
+    }
+    );
+
+const emit = defineEmits<{
+  input: [
+    UserLookupItem,
+  ];
+}>();
+
+const isOnline = computed(() =>
+    props.item.status === AgentStatus.ONLINE
+);
+
+const isOffline = computed(() =>
+      props.item.status === AgentStatus.OFFLINE ||
+      !parseUserStatus(props.item.presence)[UserStatus.SIP]
+);
+
+const isPaused = computed(() =>
+      props.item.status === AgentStatus.PAUSE ||
+      props.item.status === AgentStatus.BREAK_OUT
+);
+
+// NOTE: this computed is needed to return user status(presence) by priority
+// See this task https://my.webitel.com/browse/WTEL-3798
+const userStatus = computed(() => {
+  const status = parseUserStatus(props.item.presence);
+
+  if (status[UserStatus.DND]) return AbstractUserStatus.DND;
+  if (status[UserStatus.BUSY]) return AbstractUserStatus.BUSY;
+  if (isOnline.value) return AbstractUserStatus.ONLINE;
+  if (isOffline.value) return AbstractUserStatus.OFFLINE;
+  if (isPaused.value) return AbstractUserStatus.PAUSE;
+  if (status[UserStatus.WEB]) return AbstractUserStatus.ACTIVE;
+
+  return AbstractUserStatus.OFFLINE;
+});
+
+const handleInput = () => {
+  if (props.loading) return;
+
+  emit('input', props.item);
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
