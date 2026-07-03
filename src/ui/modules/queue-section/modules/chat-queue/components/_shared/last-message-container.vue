ccb99dc9cc052b927ade25d04b952f16581cdc56
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-07-03 18:25:40 +0300

    feat(chats): Added color display of message sender in chat for last message [WTEL-9875](https://webitel.atlassian.net/browse/WTEL-9875)

--- src/ui/modules/queue-section/modules/chat-queue/components/_shared/last-message-container.vue
+++ src/ui/modules/queue-section/modules/chat-queue/components/_shared/last-message-container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="last-message-container">
+  <div :class="['last-message-container', iconClass]">
     <div>
       <wt-icon :icon="icon" />
     </div>
@@ -8,9 +8,12 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { AgentIconType } from '../../enums/AgentIconType.enum'
+
 const props = withDefaults(
 	defineProps<{
-		icon: string;
+		icon: AgentIconType | string;
 		message: string;
 	}>(),
 	{
@@ -18,6 +21,8 @@ const props = withDefaults(
 		message: '',
 	},
 );
+
+const iconClass = computed(() => props.icon !== AgentIconType.Bot ? `last-message-container__${props.icon}` : null);
 </script>
 
 <style scoped>
@@ -25,5 +30,14 @@ const props = withDefaults(
   display: flex;
   align-items: center;
   gap: var(--spacing-xs);
+  border-radius: var(--spacing-xs);
+}
+
+.last-message-container__contacts {
+  background-color: var(--p-primary-highlight-color);
+}
+
+.last-message-container__agent {
+  background-color: var(--p-secondary-hover-color);
 }
 </style>
---
+++ src/ui/modules/queue-section/modules/chat-queue/enums/AgentIconType.enum.ts
@@ -0,0 +1,8 @@
+export const AgentIconType = {
+  Bot: 'bot',
+  Contact: 'contacts',
+  Agent: 'agent',
+} as const;
+
+export type AgentIconType =
+  (typeof AgentIconType)[keyof typeof AgentIconType]
