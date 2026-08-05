3b51c6d98f36e3e37434d90caec089136006e546
PolinaSukhorukova-webitel <p.sukhorukova@webitel.com>
2026-08-05 11:15:46 +0300

    feat: change time format in self-assigned calls[WTEL-10075](https://webitel.atlassian.net/browse/WTEL-10075)

--- src/ui/modules/queue-section/modules/call-queue/components/manual-queue/manual-queue-preview.vue
+++ src/ui/modules/queue-section/modules/call-queue/components/manual-queue/manual-queue-preview.vue
@@ -127,12 +127,15 @@ const emit = defineEmits([
 
 const wait = computed(() => {
 	const waitTime = props.task.wait;
-	const minutes = Math.floor(waitTime / 60);
-	let seconds = waitTime % 60;
-	if (seconds < 10) {
-		seconds = `0${seconds}`;
-	}
-	return `${minutes}:${seconds}`;
+
+	const hours = Math.floor(waitTime / 3600);
+	const minutes = Math.floor((waitTime % 3600) / 60);
+	const seconds = Math.floor(waitTime % 60);
+
+	const mm = String(minutes).padStart(2, '0');
+	const ss = String(seconds).padStart(2, '0');
+
+	return `${hours}:${mm}:${ss}`;
 });
 
 function accept() {
