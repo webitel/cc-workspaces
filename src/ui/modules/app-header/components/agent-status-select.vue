07305c04e9a8c12dafeee99f2ccc7264c1e5cc86
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-08-11 17:29:59 +0300

    Feat(online type): Added logic for updating call center mode from agent-status-select [WTEL-10040](https://webitel.atlassian.net/browse/WTEL-10040)

--- src/ui/modules/app-header/components/agent-status-select.vue
+++ src/ui/modules/app-header/components/agent-status-select.vue
@@ -1,19 +1,23 @@
 <template>
   <wt-cc-agent-status-select
     class="agent-status-select"
+    is-workspace
+    :is-call-center-on="isCcenterOn"
     :agent-id="agent.agentId"
     :status="agent.status"
     :status-duration="statusDuration"
+    @changed-call-center-mode="$emit('changed-call-center-mode', $event)"
   ></wt-cc-agent-status-select>
 </template>
 
 <script>
 import WtCcAgentStatusSelect from '@webitel/ui-sdk/src/modules/AgentStatusSelect/components/wt-cc-agent-status-select.vue';
 import convertDuration from '@webitel/ui-sdk/src/scripts/convertDuration';
-import { mapState } from 'vuex';
+import { mapGetters, mapState} from 'vuex';
 
 export default {
 	name: 'AgentStatusSelect',
+  emits: ['changed-call-center-mode'],
 	components: {
 		WtCcAgentStatusSelect,
 	},
@@ -26,6 +30,10 @@ export default {
 		...mapState('features/status', {
 			agent: (state) => state.agent,
 		}),
+
+    ...mapGetters('features/status', {
+      isCcenterOn: 'IS_CCENTER_ON'
+    }),
 
 		statusDuration() {
 			let time = this.now - (this.agent.lastStatusChange || Date.now());
