ca1d4d4de164ca61a6958d96f6191e3a9879b1c7
Олександр Пальонний <s2567k@gmail.com>
2026-08-28 22:52:52 +0300

    fix: fixed settings request bug [WTEL-10259](https://webitel.atlassian.net/browse/WTEL-10259)

--- src/app/api/instance.js
+++ src/app/api/instance.js
@@ -1,6 +1,11 @@
 import generateInstance from '@webitel/ui-sdk/src/api/axios/generateInstance';
 import updateTokenInterceptor from '@webitel/ui-sdk/src/api/interceptors/request/updateToken.interceptor';
 import handleUnauthorizedInterceptor from '@webitel/ui-sdk/src/api/interceptors/response/handleUnauthorized.interceptor';
+import * as qs from 'qs-esm';
+
+const stringifyOptions = {
+	arrayFormat: 'repeat',
+};
 
 export default generateInstance({
 	interceptors: {
@@ -12,4 +17,7 @@ export default generateInstance({
 		],
 	},
 	baseURL: import.meta.env.VITE_API_URL,
+	paramsSerializer: (params) => {
+		return qs.stringify(params, stringifyOptions);
+	},
 });
