f11817e9985467c35116eed0a0f7c90b1c940568
Олександр Пальонний <s2567k@gmail.com>
2026-08-31 12:29:08 +0300

    fix: rewrite for getDefaultInstance [WTEL-10259](https://webitel.atlassian.net/browse/WTEL-10259)

--- src/app/api/instance.js
+++ src/app/api/instance.js
@@ -1,23 +1,6 @@
-import generateInstance from '@webitel/ui-sdk/src/api/axios/generateInstance';
-import updateTokenInterceptor from '@webitel/ui-sdk/src/api/interceptors/request/updateToken.interceptor';
-import handleUnauthorizedInterceptor from '@webitel/ui-sdk/src/api/interceptors/response/handleUnauthorized.interceptor';
-import * as qs from 'qs-esm';
+import { getDefaultInstance } from '@webitel/api-services/api/defaults';
 
-const stringifyOptions = {
-	arrayFormat: 'repeat',
-};
+export const instance = getDefaultInstance();
 
-export default generateInstance({
-	interceptors: {
-		request: [
-			updateTokenInterceptor,
-		],
-		response: [
-			handleUnauthorizedInterceptor,
-		],
-	},
-	baseURL: import.meta.env.VITE_API_URL,
-	paramsSerializer: (params) => {
-		return qs.stringify(params, stringifyOptions);
-	},
-});
+// compat. prefer named export
+export default instance;
